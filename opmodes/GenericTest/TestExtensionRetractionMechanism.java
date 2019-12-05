ba0485f3f89a3f2cf8b89d114f99101577c7b902
TullyNYGuy <glenn_ball@utilitek.com>
2019-12-05 12:29:37 -0500

    Initial debug of reset, extension and retraction complete. Did not test interrupted commands yet.

--- opmodes/GenericTest/TestExtensionRetractionMechanism.java
+++ opmodes/GenericTest/TestExtensionRetractionMechanism.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
@@ -21,6 +22,10 @@ public class TestExtensionRetractionMechanism extends LinearOpMode {
     public ExtensionRetractionMechanism extensionRetractionMechanism;
     public DataLogging logFile;
     public double spoolDiameter = 1.25 *25.4;
+    public ElapsedTime timer;
+    public double startTime = 0;
+    public double endUpTime = 0;
+    public double endDownTime = 0;
 
     @Override
     public void runOpMode() {
@@ -31,11 +36,12 @@ public class TestExtensionRetractionMechanism extends LinearOpMode {
                 "extensionLimitSwitch", "retractionLimitSwitch", "extensionRetractionMotor",
                 DcMotor8863.MotorType.ANDYMARK_40, spoolDiameter * Math.PI);
         logFile = new DataLogging("ExtensionRetractionTest", telemetry);
+        timer = new ElapsedTime();
         extensionRetractionMechanism.setDataLog(logFile);
         extensionRetractionMechanism.enableDataLogging();
         extensionRetractionMechanism.setResetPower(+0.1);
-        extensionRetractionMechanism.setRetractionPower(+0.1);
-        extensionRetractionMechanism.setExtensionPower(-0.5);
+        extensionRetractionMechanism.setRetractionPower(+1.0);
+        extensionRetractionMechanism.setExtensionPower(-1.0);
 
         // Wait for the start button
         telemetry.addData(">", "Press Start to run" );
@@ -45,9 +51,13 @@ public class TestExtensionRetractionMechanism extends LinearOpMode {
         // Put your calls here - they will not run in a loop
         extensionRetractionMechanism.testReset(this);
         sleep(3000);
+        timer.reset();
         extensionRetractionMechanism.testExtension(this);
+        endUpTime = timer.milliseconds();
         sleep(3000);
+        timer.reset();
         extensionRetractionMechanism.testRetraction(this);
+        endDownTime = timer.milliseconds();
 
 //        while(opModeIsActive()) {
 //
@@ -61,8 +71,10 @@ public class TestExtensionRetractionMechanism extends LinearOpMode {
 //        }
 
         // Put your cleanup code here - it runs as the application shuts down
+        telemetry.addData("time up = ", endUpTime);
+        telemetry.addData("time down = ", endDownTime);
         telemetry.addData(">", "Done");
         telemetry.update();
-
+        sleep(5000);
     }
 }
