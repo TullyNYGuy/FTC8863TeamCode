3ec224ec0d3534dd8353e9be69f8e8ada5d621dd
akgh1 <54604447+akgh1@users.noreply.github.com>
2020-01-20 16:38:40 -0500

    Added Configuration class

--- Lib/FTCLib/Configuration.java
+++ Lib/FTCLib/Configuration.java
@@ -1,13 +1,36 @@
 package org.firstinspires.ftc.teamcode.Lib.FTCLib;
 
+import android.os.Environment;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
-public class Configuration {
+public class Configuration extends Properties {
     final public String CONFIG_DIRECTORY = "FTC8863";
-    final public String CONFIG_FILE = ""
-    Properties prop;
+    final public String CONFIG_FILE = "configuration.properties";
 
     public Configuration() {
-
+        super();
     }
+
+    public void load() throws IOException {
+        load(CONFIG_FILE);
+    }
+
+    public void load(String configFile) throws IOException {
+        String fileName = Environment.getExternalStorageDirectory() + CONFIG_DIRECTORY + "/" + configFile;
+        super.load(new FileInputStream(fileName));
+    }
+
+    public void store() throws IOException {
+        store(CONFIG_FILE);
+    }
+
+    public void store(String configFile) throws IOException {
+        String fileName = Environment.getExternalStorageDirectory() + CONFIG_DIRECTORY + "/" + configFile;
+        super.store(new FileOutputStream(fileName), "Robot Configuration");
+    }
+
 }
