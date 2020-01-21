7da06f3174771841041f6c7d252380af41a7133c
TullyNYGuy <glenn_ball@utilitek.com>
2020-01-20 20:31:09 -0500

    Renamed odometry variables and comments to correspond to actual odometry module positions: Right Left and Back

--- opmodes/GenericTest/Read3OdometryModules.java
+++ opmodes/GenericTest/Read3OdometryModules.java
@@ -7,53 +7,53 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.OdometryModule;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.Units;
 
-@TeleOp(name = "AAAAAAAAAAH odometry test", group = "Test")
+@TeleOp(name = "Test 3 odometry modules", group = "Test")
 //@Disabled
 public class Read3OdometryModules extends LinearOpMode {
     //Odometry Wheels
 
 
-    double odometryEncoderValue1 = 0;
-    double odometryEncoderValue2 = 0;
-    double odometryEncoderValue3 = 0;
+    double odometryModuleRightValue = 0;
+    double odometryModuleBackValue = 0;
+    double odometryModuleLeftValue = 0;
 
-    OdometryModule odometryModule1;
-    OdometryModule odometryModule2;
-    OdometryModule odometryModule3;
+    OdometryModule odometryModuleRight;
+    OdometryModule odometryModuleBack;
+    OdometryModule odometryModuleLeft;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        odometryModule1 = new OdometryModule(OdometryModule.Position.LEFT, 1440, 3.8, Units.CM, "BackRight", hardwareMap);
-        odometryModule2 = new OdometryModule(OdometryModule.Position.RIGHT, 1440, 3.8, Units.CM, "FrontRight", hardwareMap);
-        odometryModule3 = new OdometryModule(OdometryModule.Position.FRONT, 1440, 3.8, Units.CM, "BackLeft", hardwareMap);
+        odometryModuleRight = new OdometryModule(1440, 3.8, Units.CM, "BackRight", hardwareMap);
+        odometryModuleBack = new OdometryModule(1440, 3.8, Units.CM, "FrontRight", hardwareMap);
+        odometryModuleLeft = new OdometryModule(1440, 3.8, Units.CM, "BackLeft", hardwareMap);
 
         //Odometry System Calibration Init Complete
         telemetry.addData("Odometry System Calibration Status", "Init Complete");
-        odometryEncoderValue1 = odometryModule1.getDistanceSinceReset(Units.CM);
-        odometryEncoderValue2 = odometryModule2.getDistanceSinceReset(Units.CM);
-        odometryEncoderValue3 = odometryModule3.getDistanceSinceReset(Units.CM);
-        telemetry.addData("Odometry encoder 1 value = ", odometryEncoderValue1);
-        telemetry.addData("Odometry encoder 2 value = ", odometryEncoderValue2);
-        telemetry.addData("Odometry encoder 3 value = ", odometryEncoderValue3);
+        odometryModuleRightValue = odometryModuleRight.getDistanceSinceReset(Units.CM);
+        odometryModuleBackValue = odometryModuleBack.getDistanceSinceReset(Units.CM);
+        odometryModuleLeftValue = odometryModuleLeft.getDistanceSinceReset(Units.CM);
+        telemetry.addData("Odometry encoder 1 value = ", odometryModuleRightValue);
+        telemetry.addData("Odometry encoder 2 value = ", odometryModuleBackValue);
+        telemetry.addData("Odometry encoder 3 value = ", odometryModuleLeftValue);
         telemetry.update();
 
 
         waitForStart();
 
         while (opModeIsActive()) {
-            odometryEncoderValue1 = odometryModule1.getEncoderValue();
-            odometryEncoderValue2 = odometryModule2.getEncoderValue();
-            odometryEncoderValue3 = odometryModule3.getEncoderValue();
-            telemetry.addData("Odometry encoder value1 = ", odometryEncoderValue1);
-            telemetry.addData("Odometry Encoder Value1 (In)", String.format("%.2f", odometryEncoderValue1 / 1440 * 1.5 * Math.PI));
-            telemetry.addData("Odometry Encoder Value1 (cm)", String.format("%.2f", odometryModule1.getDistanceSinceReset(Units.CM)));
-            telemetry.addData("Odometry encoder value2 = ", odometryEncoderValue2);
-            telemetry.addData("Odometry Encoder Value2 (In)", String.format("%.2f", odometryEncoderValue2 / 1440 * 1.5 * Math.PI));
-            telemetry.addData("Odometry Encoder Value2 (cm)", String.format("%.2f", odometryModule2.getDistanceSinceReset(Units.CM)));
-            telemetry.addData("Odometry encoder value3 = ", odometryEncoderValue3);
-            telemetry.addData("Odometry Encoder Value3 (In)", String.format("%.2f", odometryEncoderValue3 / 1440 * 1.5 * Math.PI));
-            telemetry.addData("Odometry Encoder Value3 (cm)", String.format("%.2f", odometryModule3.getDistanceSinceReset(Units.CM)));
+            odometryModuleRightValue = odometryModuleRight.getEncoderValue();
+            odometryModuleBackValue = odometryModuleBack.getEncoderValue();
+            odometryModuleLeftValue = odometryModuleLeft.getEncoderValue();
+            telemetry.addData("Odometry encoder Right = ", odometryModuleRightValue);
+            telemetry.addData("Odometry Encoder Right (In)", String.format("%.2f", odometryModuleRightValue / 1440 * 1.5 * Math.PI));
+            telemetry.addData("Odometry Encoder Right (cm)", String.format("%.2f", odometryModuleRight.getDistanceSinceReset(Units.CM)));
+            telemetry.addData("Odometry encoder Back = ", odometryModuleBackValue);
+            telemetry.addData("Odometry Encoder Back (In)", String.format("%.2f", odometryModuleBackValue / 1440 * 1.5 * Math.PI));
+            telemetry.addData("Odometry Encoder Back (cm)", String.format("%.2f", odometryModuleBack.getDistanceSinceReset(Units.CM)));
+            telemetry.addData("Odometry encoder Left = ", odometryModuleLeftValue);
+            telemetry.addData("Odometry Encoder Left (In)", String.format("%.2f", odometryModuleLeftValue / 1440 * 1.5 * Math.PI));
+            telemetry.addData("Odometry Encoder Left (cm)", String.format("%.2f", odometryModuleLeft.getDistanceSinceReset(Units.CM)));
             //Update values
             telemetry.update();
         }
