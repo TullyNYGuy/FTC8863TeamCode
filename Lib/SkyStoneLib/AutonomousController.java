b3882b5ddbee0d08eee7418db77edb70aa5a8279
akgh1 <54604447+akgh1@users.noreply.github.com>
2020-01-26 02:24:00 -0500

    - Implemented moveTo for autonomous mode - Implemented movement thread - Robot initialization

--- Lib/SkyStoneLib/AutonomousController.java
+++ Lib/SkyStoneLib/AutonomousController.java
@@ -2,12 +2,19 @@ package org.firstinspires.ftc.teamcode.Lib.SkyStoneLib;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.MecanumCommands;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.OdometryModule;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.PIDControl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 
 public class AutonomousController {
@@ -30,6 +37,11 @@ public class AutonomousController {
 
     final private DistanceUnit distanceUnit = DistanceUnit.CM;
 
+    /*
+     * Interval in milliseconds in which movement control task runs
+     */
+    final private long MOVEMENT_THREAD_INTERVAL = 50;
+
     private enum Areas {
         BUILDSITE, BRIDGE, BLOCK, PLATFORM, HOME
     }
@@ -49,24 +61,97 @@ public class AutonomousController {
 
     private SkystoneRobot robot;
 
+    private ScheduledExecutorService scheduler;
+    private MovemenetThread movementThread;
+    private ScheduledFuture<?> movementTask;
+
+    class MovemenetThread implements Runnable {
+
+        private PIDControl xControl;
+        private PIDControl yControl;
+        private PIDControl rotationControl;
+        private MecanumCommands commands;
+        private Position current;
+        private DistanceUnit distanceUnit;
+        private MecanumCommands zeroMovement;
+
+        public MovemenetThread(DistanceUnit distanceUnit) {
+            xControl = new PIDControl(0.2, 20.0, 0);
+            yControl = new PIDControl(0.2, 20.0, 0);
+            rotationControl = new PIDControl(0.2, 20.0, 0);
+            commands = new MecanumCommands();
+            zeroMovement = new MecanumCommands();
+            this.distanceUnit = distanceUnit;
+            current = new Position(distanceUnit, 0, 0, 0, 0);
+        }
+
+        public void setDestination(DistanceUnit distanceUnit, double x, double y) {
+            synchronized (this) {
+                xControl.setSetpoint(this.distanceUnit.fromUnit(distanceUnit, x));
+                yControl.setSetpoint(this.distanceUnit.fromUnit(distanceUnit, y));
+            }
+        }
+
+        public void setDestinationRotation(AngleUnit angleUnit, double rotation) {
+            synchronized (this) {
+                rotationControl.setSetpoint(angleUnit.toRadians(rotation));
+            }
+        }
+
+        @Override
+        public void run() {
+            double valX;
+            double valY;
+            double valRot;
+            robot.getCurrentPosition(current);
+            double currentRotation = robot.getCurrentRotation(AngleUnit.RADIANS);
+            synchronized (this) {
+                valX = xControl.getCorrection(current.x);
+                valY = yControl.getCorrection(current.y);
+                valRot = rotationControl.getCorrection(currentRotation);
+            }
+            commands.setSpeed(Math.sqrt(valX * valX + valY * valY));
+            commands.setAngleOfTranslation(AngleUnit.RADIANS, Math.atan2(valY, valX));
+            commands.setSpeedOfRotation(valRot);
+            robot.setMovement(commands);
+        }
+    }
+
     public AutonomousController(SkystoneRobot robot) {
         places = new HashMap<Areas, Position>();
         this.robot = robot;
+        movementThread = new MovemenetThread(distanceUnit);
+        scheduler = Executors.newScheduledThreadPool(2);
+        movementTask = null;
     }
 
+
+    public void startController() {
+        if (movementTask == null)
+            movementTask = scheduler.scheduleAtFixedRate(movementThread, 0, MOVEMENT_THREAD_INTERVAL, TimeUnit.MILLISECONDS);
+    }
+
+    public void stopController() {
+        if (movementTask != null) {
+            while (!movementTask.isDone()) {
+                movementTask.cancel(false);
+            }
+            movementTask = null;
+        }
+    }
 
     public void setBlockState(boolean state) {
         blockState = state;
     }
 
-    public void setAlligance(Color color) {
+    public void setAllegiance(Color color) {
         this.color = color;
     }
 
     // these variables are place holders for now. in the furture they will be only the coordinates for the first
     // block in the line
-    public void moveTo(double x, double y) {
-        //move robot to specified coordinates
+    public void moveTo(DistanceUnit distanceUnit, double x, double y) {
+        movementThread.setDestination(distanceUnit, x, y);
     }
 
     public void initPlaces() {
@@ -87,7 +172,7 @@ public class AutonomousController {
         this.place = place;
         Position p = places.get(place);
         if (p != null) {
-            moveTo(p.x, p.y);
+            moveTo(distanceUnit, p.x, p.y);
         }
     }
 
--- Lib/SkyStoneLib/SkystoneRobot.java
+++ Lib/SkyStoneLib/SkystoneRobot.java
@@ -1,64 +1,158 @@
 package org.firstinspires.ftc.teamcode.Lib.SkyStoneLib;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.AdafruitIMU8863;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.Configuration;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.Mecanum;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.MecanumCommands;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.OdometryModule;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.OdometrySystem;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.PIDControl;
+
+import static org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863.MotorType.ANDYMARK_20_ORBITAL;
 
 public class SkystoneRobot {
-    /* TODO: Needs initialization */
+
+    final public String PROP_IMU_NAME = "imu.deviceName";
+
+    HardwareMap hardwareMap;
+    Telemetry telemetry;
+    DistanceUnit units;
+    Configuration config;
+
+    private AdafruitIMU8863 imu;
     private IntakeWheels intake;
     private Mecanum mecanum;
+    private OdometrySystem odometry;
+
+    /* TODO: Needs initialization */
     private DualLift lift;
     private ExtensionArm extensionArm;
     private GripperRotator gripper;
-    private OdometrySystem odometry;
 
-    public IntakeWheels getIntake() {
-        return intake;
+    public SkystoneRobot(HardwareMap hardwareMap, Telemetry telemetry, Configuration config, DistanceUnit units) {
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+        this.units = units;
+        this.config = config;
     }
 
-    public void setIntake(IntakeWheels intake) {
-        this.intake = intake;
-    }
+    boolean initialize() {
+        DcMotor8863 frontLeft = new DcMotor8863("FrontLeft", hardwareMap);
+        DcMotor8863 backLeft = new DcMotor8863("BackLeft", hardwareMap);
+        DcMotor8863 frontRight = new DcMotor8863("FrontRight", hardwareMap);
+        DcMotor8863 backRight = new DcMotor8863("BackRight", hardwareMap);
 
-    public Mecanum getMecanum() {
-        return mecanum;
-    }
-
-    public void setMecanum(Mecanum mecanum) {
-        this.mecanum = mecanum;
-    }
-
-    public DualLift getLift() {
-        return lift;
-    }
-
-    public void setLift(DualLift lift) {
-        this.lift = lift;
-    }
-
-    public ExtensionArm getExtensionArm() {
-        return extensionArm;
-    }
-
-    public void setExtensionArm(ExtensionArm extensionArm) {
-        this.extensionArm = extensionArm;
-    }
-
-    public GripperRotator getGripper() {
-        return gripper;
-    }
-
-    public void setGripper(GripperRotator gripper) {
-        this.gripper = gripper;
-    }
+        // these motors are orbital (planetary gear) motors. The type of motor sets up the number
+        // of encoder ticks per revolution. Since we are not using encoder feedback yet, this is
+        // really not important now. But it will be once we hook up the encoders and set a motor
+        // mode that uses feedback.
+        frontLeft.setMotorType(ANDYMARK_20_ORBITAL);
+        backLeft.setMotorType(ANDYMARK_20_ORBITAL);
+        frontRight.setMotorType(ANDYMARK_20_ORBITAL);
+        backRight.setMotorType(ANDYMARK_20_ORBITAL);
 
 
-    public SkystoneRobot() {
+        // This value will get set to some distance traveled per revolution later.
+        frontLeft.setMovementPerRev(360);
+        backLeft.setMovementPerRev(360);
+        frontRight.setMovementPerRev(360);
+        backRight.setMovementPerRev(360);
 
+        // The encoder tolerance is used when you give the motor a target encoder tick count to rotate to. 5 is
+        // probably too tight. 10 is pretty good based on experience. Note that 10 is set as the
+        // default when you create a motor object so this statement is not needed.
+        //frontLeft.setTargetEncoderTolerance(10);
+
+        // FLOAT  is also the default when you create a new motor object
+        //frontLeft.setFinishBehavior(DcMotor8863.FinishBehavior.FLOAT);
+
+        // powers are also defaulted to -1 and 1
+        //frontLeft.setMinMotorPower(-1);
+        //frontLeft.setMaxMotorPower(1);
+
+        // setDirection() is a software control that controls which direction the motor moves when
+        // you give it a positive power. We may have to change this once we see which direction the
+        // motor actually moves.
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // set the running mode for the motor. The motor initializes at STOP_AND_RESET_ENCODER which
+        // resets the encoder count to zero. After this you have to choose a mode that will allow
+        // the motor to run.
+        // In this case, we do not have the encoder connected from the motor. So we only have one
+        // choice. We must run the motor without any feedback (open loop). This call is not really
+        // needed since later I use runAtConstantPower() and that sets the mode too. But since you
+        // are coming up to speed on the motors, I put this here for you to see (like my pun?).
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // The other 2 options would be:
+        // RUN_TO_POSITION - run until the targeted encoder count is reached using PID
+        // RUN_WITH_ENCODER - run at a velocity controlled by a PID
+        // For more details, see this page and start reading at Running the motor and continue down
+        // https://ftc-tricks.com/dc-motors/
+
+        // Make sure the motor does not start moving. This is not really needed because
+        // runAtConstantPower(0) below does the same thing. But I put it here so you can see this
+        // call exists.
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+
+        // The runAtConstantPower() and runAtConstantSpeed() methods setup the motor to do that.
+        // They are initialzation methods. So they should not be inside the while loop.
+        //
+        // We can't use runAtConstantSpeed because there is no encoder feedback. I suspect this
+        // is why the motor did not turn. runAtConstantSpeed uses the encoder and PID control
+        // to turn the motor at a constant velocity.
+        //frontLeft.runAtConstantSpeed(mecanum.getFrontLeft());
+        //
+        // Instead we will run the motor open loop (without controlling its speed, just feeding
+        // it a power. Initialize the motor power to 0 for now.
+        frontLeft.runAtConstantPower(0);
+        backLeft.runAtConstantPower(0);
+        frontRight.runAtConstantPower(0);
+        backRight.runAtConstantPower(0);
+
+        imu = new AdafruitIMU8863(hardwareMap, null, "IMU", config.getProperty(PROP_IMU_NAME, "IMU"));
+        mecanum = new Mecanum(frontLeft, frontRight, backLeft, backRight, telemetry);
+        OdometryModule left = new OdometryModule(1440, 3.8, units, "BackLeft", hardwareMap);
+        OdometryModule right = new OdometryModule(1440, 3.8, units, "BackRight", hardwareMap);
+        OdometryModule back = new OdometryModule(1440, 3.8, units, "FrontRight", hardwareMap);
+        odometry = new OdometrySystem(units, left, right, back);
+        if (!odometry.loadConfiguration(config)) {
+            telemetry.addData("ERROR", "Couldn't initialize Odometry");
+            return false;
+        }
+        DcMotor8863 rightIntake = new DcMotor8863("intakeMotorRight", hardwareMap);
+        DcMotor8863 leftIntake = new DcMotor8863("intakeMotorLeft", hardwareMap);
+        rightIntake.setMotorType(ANDYMARK_20_ORBITAL);
+        leftIntake.setMotorType(ANDYMARK_20_ORBITAL);
+        intake = new IntakeWheels(rightIntake, leftIntake);
+        return true;
     }
 
     void getCurrentPosition(Position position) {
         odometry.getCurrentPosition(position);
+    }
+
+    double getCurrentRotation(AngleUnit unit) {
+        return odometry.getCurrentRotation(unit);
+    }
+
+    void setMovement(MecanumCommands commands) {
+        mecanum.setMotorPower(commands);
     }
 }
