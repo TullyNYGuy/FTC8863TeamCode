92d47499d0e6dbc7283a3d8dabd4cd93e6380914
akgh1 <54604447+akgh1@users.noreply.github.com>
2020-01-30 19:43:46 -0500

    Update opmode name for TestMecanumWithDrivetrain

--- opmodes/GenericTest/TestMecanumToDrivetrain.java
+++ opmodes/GenericTest/TestMecanumToDrivetrain.java
@@ -25,7 +25,7 @@ import static org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863.MotorType.AN
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
  */
-@TeleOp(name = "Mecanum with intake", group = "Run")
+@TeleOp(name = "Test Mecanum To Drivetrain", group = "Test")
 //@Disabled
 public class TestMecanumToDrivetrain extends LinearOpMode {
 
