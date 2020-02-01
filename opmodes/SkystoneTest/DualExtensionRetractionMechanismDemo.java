67e267c8c7d9499455ed3a0bf9662f3259bd08f4
TullyNYGuy <glenn_ball@utilitek.com>
2020-02-01 07:04:09 -0500

    DualExtensionRetractionMechanismDemo - moved to SkystoneTest, disabled TwoLiftDemo - moved to SkystoneTest, disabled

--- opmodes/SkystoneTest/DualExtensionRetractionMechanismDemo.java
+++ opmodes/SkystoneTest/DualExtensionRetractionMechanismDemo.java
@@ -1,5 +1,6 @@
-package org.firstinspires.ftc.teamcode.opmodes.Skystone;
+package org.firstinspires.ftc.teamcode.opmodes.SkystoneTest;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -13,8 +14,8 @@ import org.firstinspires.ftc.teamcode.Lib.SkyStoneLib.Lift;
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
  */
-//@TeleOp(name = "AAA Dual Extension Retraction Demo", group = "RUN")
-//@Disabled
+@TeleOp(name = "AAA Dual Extension Retraction Demo", group = "RUN")
+@Disabled
 public class DualExtensionRetractionMechanismDemo extends LinearOpMode {
 
     // Put your variable declarations here
