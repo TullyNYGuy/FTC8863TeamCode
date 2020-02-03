086d00368ecbc772e80a8df13b47fc135244b389
TullyNYGuy <glenn_ball@utilitek.com>
2020-02-03 13:24:14 -0500

    Added opmode to record expansion hub voltage and current values while drive train motors are running

--- opmodes/GenericTest/TestRevVoltageMonitors.java
+++ opmodes/GenericTest/TestRevVoltageMonitors.java
@@ -3,8 +3,11 @@ package org.firstinspires.ftc.teamcode.opmodes.GenericTest;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.CSVDataFile;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
+import org.firstinspires.ftc.teamcode.Lib.SkyStoneLib.SkystoneRobot;
 import org.openftc.revextensions2.ExpansionHubEx;
 
 /**
@@ -20,6 +23,11 @@ public class TestRevVoltageMonitors extends LinearOpMode {
 
     CSVDataFile csvDataFile;
 
+    DcMotor8863 frontLeft;
+    DcMotor8863 frontRight;
+    DcMotor8863 backLeft;
+    DcMotor8863 backRight;
+
     @Override
     public void runOpMode() {
 
@@ -31,10 +39,28 @@ public class TestRevVoltageMonitors extends LinearOpMode {
         csvDataFile = new CSVDataFile("RevVoltageCurrentMonitors", telemetry);
         csvDataFile.headerStrings("Primary Voltage", "Secondary Voltage", "Primary Current", "Secondary Current");
 
+        ElapsedTime timer = new ElapsedTime();
+
+        frontLeft = new DcMotor8863(SkystoneRobot.HardwareName.FRONT_LEFT_MOTOR.hwName, hardwareMap);
+        frontRight = new DcMotor8863(SkystoneRobot.HardwareName.FRONT_RIGHT_MOTOR.hwName, hardwareMap);
+        backLeft = new DcMotor8863(SkystoneRobot.HardwareName.BACK_LEFT_MOTOR.hwName, hardwareMap);
+        backRight = new DcMotor8863(SkystoneRobot.HardwareName.BACK_RIGHT_MOTOR.hwName, hardwareMap);
+
+        frontLeft.setMotorType(DcMotor8863.MotorType.ANDYMARK_20_ORBITAL);
+        frontLeft.runAtConstantPower(0);
+        frontRight.setMotorType(DcMotor8863.MotorType.ANDYMARK_20_ORBITAL);
+        frontRight.runAtConstantPower(0);
+        backLeft.setMotorType(DcMotor8863.MotorType.ANDYMARK_20_ORBITAL);
+        backLeft.runAtConstantPower(0);
+        backRight.setMotorType(DcMotor8863.MotorType.ANDYMARK_20_ORBITAL);
+        backRight.runAtConstantPower(0);
+
         // Wait for the start button
         telemetry.addData(">", "Press Start to run");
         telemetry.update();
         waitForStart();
+
+        timer.reset();
 
         while (opModeIsActive()) {
             csvDataFile.writeData(
@@ -43,6 +69,22 @@ public class TestRevVoltageMonitors extends LinearOpMode {
                     expansionHubPrimary.getTotalModuleCurrentDraw((ExpansionHubEx.CurrentDrawUnits.AMPS)),
                     expansionHubSecondary.getTotalModuleCurrentDraw((ExpansionHubEx.CurrentDrawUnits.AMPS))
             );
+
+            if (timer.milliseconds() > 2000) {
+                frontLeft.setPower(1);
+                frontRight.setPower(1);
+                backLeft.setPower(1);
+                backRight.setPower(1);
+                timer.reset();
+            }
+
+            if (timer.milliseconds() > 3000) {
+                frontLeft.setPower(0);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+                backRight.setPower(0);
+            }
+
             telemetry.addData(">", "Press Stop to end test.");
             telemetry.update();
 
