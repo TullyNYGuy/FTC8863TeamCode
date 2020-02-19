1091c167d41d05de5ddf3b8902d4086036487711
TullyNYGuy <glenn_ball@utilitek.com>
2020-02-18 21:52:15 -0500

    MotorCurrentVoltageMonitor - new class to monitor the motor currents and voltage on an expansion hub TestDriveMotorWiring - modified to use the new class CSVDataFile - fixed comments

--- Lib/FTCLib/MotorCurrentVoltageMonitor.java
+++ Lib/FTCLib/MotorCurrentVoltageMonitor.java
@@ -8,6 +8,12 @@ import org.openftc.revextensions2.ExpansionHubEx;
 
 import java.util.ArrayList;
 
+/**
+ * This class gets the expansion hub voltage and motor currents for the motor attached to the
+ * expansion hub. You can display the info on the driver's station, write the info into a comma
+ * separated value file, or both. It only handles 1 expansion hub. Create 2 objects from this class
+ * if you want to monitor both expansion hubs.
+ */
 public class MotorCurrentVoltageMonitor {
 
     //*********************************************************************************************
@@ -17,13 +23,16 @@ public class MotorCurrentVoltageMonitor {
     //
     //*********************************************************************************************
 
-    public enum Mode {
+    /**
+     * Where the current and voltage data get output to.
+     */
+    public enum OutputTo {
         WRITE_CSV_FILE,
         DISPLAY,
         WRITE_CSV_FILE_AND_DISPLAY
     }
 
-    private Mode mode;
+    private OutputTo mode;
 
     //*********************************************************************************************
     //          PRIVATE DATA FIELDS
@@ -32,12 +41,27 @@ public class MotorCurrentVoltageMonitor {
     // getter and setter methods
     //*********************************************************************************************
 
+    /**
+     * Holds the list of motors that you want to get currents. Note that the max number of motors
+     * is 4 since that is how many motor ports are on an expansion hub.
+     */
     private ArrayList<DcMotor8863> motorList;
 
+    /**
+     * The expansion hub object
+     */
     private ExpansionHubEx expansionHub;
 
+    /**
+     * A data file that the current and voltage can be written into.
+     */
     private CSVDataFile dataFile;
 
+    private Telemetry telemetry;
+
+    /**
+     * An array to store the motor currents.
+     */
     private double[] motorCurrents;
     private double supplyVoltage;
 
@@ -56,8 +80,9 @@ public class MotorCurrentVoltageMonitor {
     // from it
     //*********************************************************************************************
 
-    public MotorCurrentVoltageMonitor(HardwareMap hardwareMap, Telemetry telemetry, String expansionHubName, Mode mode) {
+    public MotorCurrentVoltageMonitor(HardwareMap hardwareMap, Telemetry telemetry, String expansionHubName, OutputTo mode) {
         expansionHub = hardwareMap.get(ExpansionHubEx.class, expansionHubName);
+        this.telemetry = telemetry;
         motorList = new ArrayList<DcMotor8863>();
         this.mode = mode;
         motorCurrents = new double[4];
@@ -75,6 +100,11 @@ public class MotorCurrentVoltageMonitor {
     // public methods that give the class its functionality
     //*********************************************************************************************
 
+    /**
+     * Add a motor to the list of motors to be monitored
+     *
+     * @param motor
+     */
     public void addMotor(DcMotor8863 motor) {
         if (motorList.size() < 4) {
             motorList.add(motor);
@@ -83,32 +113,81 @@ public class MotorCurrentVoltageMonitor {
         }
     }
 
+    /**
+     * The file prefix for the data file. Date and time get appended to the name.
+     *
+     * @param filePrefix
+     */
     public void setupCSVDataFile(String filePrefix) {
         dataFile = new CSVDataFile(filePrefix);
+        writeCSVHeaderLine();
     }
 
+    /**
+     * Write a header line into the comma separated value file
+     */
     private void writeCSVHeaderLine() {
-
+        String buffer = "";
+        if (dataFile != null) {
+            for (DcMotor8863 motor : motorList) {
+                buffer = buffer + motor.getMotorName() + " current,";
+            }
+            buffer = buffer + " Expansion hub voltage";
+            dataFile.writeData(buffer);
+        }
     }
 
+    /**
+     * Write the motor current and hub voltage data just gathered to the data file.
+     */
     private void writeCSVData() {
-
+        if (dataFile != null) {
+            dataFile.writeData(motorCurrents[0], motorCurrents[1], motorCurrents[2], motorCurrents[3], supplyVoltage);
+        }
     }
 
+    /**
+     * Add the current and voltage data to the telemetry data to be displayed on the driver's station.
+     * Note that you will have to actually make it display using a telemetry.update().
+     */
     public void fillDisplayBuffer() {
-
+        int index = 0;
+        for (DcMotor8863 motor : motorList) {
+            telemetry.addData(motor.getMotorName() + " current = ", motorCurrents[index]);
+            index++;
+        }
+        telemetry.addData("hub voltage = ", supplyVoltage);
     }
 
-    private void getData(ArrayList<DcMotor8863> motorList) {
+    /**
+     * Get the motor currents and the hub voltage
+     */
+    private void getData() {
         int index = 0;
         for (DcMotor8863 motor : motorList) {
             motorCurrents[index] = expansionHub.getMotorCurrentDraw(ExpansionHubEx.CurrentDrawUnits.AMPS, motor.getPortNumber());
+            index++;
         }
         supplyVoltage = expansionHub.read12vMonitor(ExpansionHubEx.VoltageUnits.VOLTS);
     }
 
+    /**
+     * Update the current and voltage data and output it based on the mode.
+     */
     public void update() {
-
+        getData();
+        if (mode == OutputTo.DISPLAY || mode == OutputTo.WRITE_CSV_FILE_AND_DISPLAY) {
+            fillDisplayBuffer();
+        }
+        if (mode == OutputTo.WRITE_CSV_FILE || mode == OutputTo.WRITE_CSV_FILE_AND_DISPLAY) {
+            writeCSVData();
+        }
     }
 
+    /**
+     * Close the data file when you are done storing data
+     */
+    public void closeCSVData() {
+        dataFile.closeDataLog();
+    }
 }
