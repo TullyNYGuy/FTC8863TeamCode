43d45941d7565d6c4df0f8b88f99f7d5d8268a2b
tatyk <tatyklochko@gmail.com>
2020-02-28 18:44:24 -0500

    made a function to check if the destination is reached for time and distance traveled. made a class to at least be a template for the most complicated version of the autonomous made a class to test the accuracy of the odometry system

--- opmodes/SkystoneTest/OdometryAccuracyTest.java
+++ opmodes/SkystoneTest/OdometryAccuracyTest.java
@@ -2,23 +2,16 @@ package org.firstinspires.ftc.teamcode.opmodes.SkystoneTest;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.AdafruitIMU8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.Configuration;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
-import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
-import org.firstinspires.ftc.teamcode.Lib.FTCLib.Mecanum;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.MecanumCommands;
 import org.firstinspires.ftc.teamcode.Lib.SkyStoneLib.AutonomousController;
-import org.firstinspires.ftc.teamcode.Lib.SkyStoneLib.IntakeWheels;
 import org.firstinspires.ftc.teamcode.Lib.SkyStoneLib.SkystoneRobot;
-
-import static org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863.MotorType.ANDYMARK_20_ORBITAL;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
@@ -26,7 +19,7 @@ import static org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863.MotorType.AN
 @Autonomous(name = "Super Complicated Autonomous", group = "A Test")
 //@Disabled
 
-public class AunomouseComplicated extends LinearOpMode {
+public class OdometryAccuracyTest extends LinearOpMode {
 
     // Put your variable declarations here
     public SkystoneRobot robot;
@@ -82,44 +75,31 @@ public class AunomouseComplicated extends LinearOpMode {
             idle();
         }
         controller.startController();
-robot.setPosition(0,0,0);
-controller.setAllegiance(AutonomousController.Color.RED);
-controller.initPlaces();
+        robot.setPosition(0,0,0);
+        controller.setAllegiance(AutonomousController.Color.RED);
+        controller.initPlaces();
         waitForStart();
-controller.goTo(AutonomousController.Areas.BLOCK);
-while(controller.isActionCompleteTime() != true){
-    idle();
-}
-controller.pickUpBlock();
-controller.goTo(AutonomousController.Areas.BRIDGE);
-        while(controller.isActionCompleteTime() != true){
+        controller.moveTo(DistanceUnit.CM, 50, 0);
+        outtakeTimer.reset();
+        while(outtakeTimer.seconds() > 5){
             idle();
         }
-controller.goTo(AutonomousController.Areas.PLATFORM);
-        while(controller.isActionCompleteTime() != true){
+        controller.moveTo(DistanceUnit.CM, 0, 50);
+        outtakeTimer.reset();
+        while(outtakeTimer.seconds() > 5){
             idle();
         }
-controller.putBlockOnBase();
-controller.moveBaseRotate();
-controller.goTo(AutonomousController.Areas.BRIDGE);
-        while(controller.isActionCompleteTime() != true){
+        controller.moveTo(DistanceUnit.CM, -50, 0);
+        outtakeTimer.reset();
+        while(outtakeTimer.seconds() > 5){
             idle();
         }
-controller.goTo(AutonomousController.Areas.BLOCK);
-        while(controller.isActionCompleteTime() != true){
+        controller.moveTo(DistanceUnit.CM, 0, -50);
+        outtakeTimer.reset();
+        while(outtakeTimer.seconds() > 5){
             idle();
         }
-controller.pickUpBlock();
-controller.goTo(AutonomousController.Areas.BRIDGE);
-        while(controller.isActionCompleteTime() != true){
-            idle();
-        }
-controller.goTo(AutonomousController.Areas.PLATFORM);
-        while(controller.isActionCompleteTime() != true){
-            idle();
-        }
-controller.putBlockOnBase();
-controller.parkWithArm();
+
         // Put your cleanup code here - it runs as the application shuts down
         telemetry.addData(">", "Done");
         dataLog.closeDataLog();
