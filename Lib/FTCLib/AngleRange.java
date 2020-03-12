9fe8f74b0fbbfb6db7959c3347e14b821b5baa52
TullyNYGuy <glenn_ball@utilitek.com>
2020-03-11 23:44:04 -0400

    working on AngleRange

--- Lib/FTCLib/AngleRange.java
+++ Lib/FTCLib/AngleRange.java
@@ -1,23 +1,5 @@
 package org.firstinspires.ftc.teamcode.Lib.FTCLib;
 
-/**
- * This enum represents the range for an angle.
- * <p>
- * An angle of 0 can be defined to be at one of 4 of the axes. The four axes are what we think of as
- * +x +y -x and -y. Most commonly, in our robots, 0 degrees is the + y axis. By comparison most of
- * us think of +x as the 0 axis.
- * <p>
- * +y
- * ^
- * |
- * |
- * -x <----------|------------> +x
- * |
- * |
- * -y
- * <p>
- * This enum translates between different reference axes.
- */
 
 //*********************************************************************************************
 //          ENUMERATED TYPES
@@ -32,6 +14,48 @@ package org.firstinspires.ftc.teamcode.Lib.FTCLib;
  * with its range. And it translates between ranges.
  */
 
+// 0 to 360 range
+//                0
+//                ^
+//                |
+//                |
+//  270<----------|------------> 90
+//                |
+//                |
+//               180
+//
+// negative angles rotate counter clockwise. i.e.
+// -90 = 270
+// -180 = 180
+// -270 = 90
+
+// 0 to -360 range
+//                 0
+//                 ^
+//                 |
+//                 |
+//  -90 <----------|------------> -270
+//                 |
+//                 |
+//               -180
+//
+// positive angles rotate clockwise i.e.
+// +90 = -270
+// +180 = -180
+// +270 = -90
+
+// -180 to 180 range
+//                0
+//                ^
+//                |
+//                |
+// -90 <----------|------------> 90
+//                |
+//                |
+//            -179 +179
+
+
+
 public enum AngleRange {
     PLUS_TO_MINUS_180(0),
     ZERO_TO_PLUS_360(1),
@@ -44,40 +68,171 @@ public enum AngleRange {
     }
 
     /**
-     * Translate that given angle, which is in the range of 0 to 360, to the range in this enum.
+     * Take any angle, positive or negative, and rotate it into the range of 0 to 360 degrees. The
+     * assumption is that the angle is supposed to be in the range of 0 to 360 to start with. But
+     * maybe it wrapped past 360 (like 720) or 0 (like -360) to get it out of the range.
+     *
+     * @param angle
+     * @return
+     */
+    public double normalizeToZeroTo360(double angle) {
+        // I'm not going to trust that the angle passed to this method is actually in the range
+        // of 0 to 360. So put it in the range of -360 to +360. Use the remainder operator (modulo)
+        // to do that.
+        // example:
+        // -450 % 360 = -90
+        // +720 % 360 = 0
+        angle = angle % 360;
+        // now get the negative angles back to where they should be
+        // if the angle is < 0 (negative) translate it to a positive angle so it ends up in the
+        // (0 to +360)
+        if (angle < 0) {
+            angle += 360;
+        }
+        return angle;
+    }
+
+    /**
+     * Take any angle, positive or negative, and rotate it into the range of 0 to -360 degrees. The
+     * assumption is that the angle is supposed to be in the range of 0 to -360 to start with. But
+     * maybe it wrapped past -360 (like -720) or 0 (like +360) to get it out of the range.
+     *
+     * @param angle
+     * @return
+     */
+    public double normalizeToZeroToMinus360(double angle) {
+        // I'm not going to trust that the angle passed to this method is actually in the range
+        // of 0 to -360. So put it in the range of -360 to +360. Use the remainder operator (modulo)
+        // to do that.
+        // example:
+        // -450 % 360 = -90
+        // +720 % 360 = 0
+        angle = angle % 360;
+        // now get the negative angles back to where they should be
+        // if the angle is < 0 (negative) translate it to a positive angle so it ends up in the
+        // (0 to +360)
+        if (angle > 0) {
+            angle -= 360;
+        }
+        return angle;
+    }
+
+    public double normalizeToPlusToMinus180(double angle) {
+        // I'm not going to trust that the angle passed to this method is actually in the range
+        // of -180 to +180. So put it in the range of -360 to +360. Use the remainder operator (modulo)
+        // to do that.
+        // example:
+        // -450 % 360 = -90
+        // +720 % 360 = 0
+        // ToDo essentially the task is to translate any negative or positive angle to -180 to +180 range
+        // starting point? angle = angle % 180;
+    }
+
+    /**
+     * Translate the given angle, which is in the range of 0 to 360, to the range in this enum.
      * @param angle
      * @return
      */
     public double fromZeroToPlus360(double angle) {
+        // I'm not going to trust that the angle passed to this method is actually in the range
+        // of 0 to 360. So
+        angle = normalizeToZeroTo360(angle);
+
+        // now that the angle is actually in the 0 - +360 range, translate it to one of these
+        // cases
+        switch (this) {
+            case ZERO_TO_PLUS_360:
+                // the angle is already 0 to +360, don't do anything to translated angle.
+                break;
+            case ZERO_TO_MINUS_360:
+                // the angle is 0 to +360, subtract 360 to get it into -360 to 0 range
+                angle -= 360;
+                break;
+            case PLUS_TO_MINUS_180:
+                //ToDo need an algorithm here
+                // maybe? angle = (angle + 360) % 360;
+                break;
+        }
+        return angle;
+    }
+
+    /**
+     * Translate the given angle, which is in the range of -180 to +180, to the range in this enum.
+     *
+     * @param angle
+     * @return
+     */
+    public double fromPlusToMinus180(double angle) {
+        // I'm not going to trust that the angle passed to this method is actually in the range
+        // of -180 to + 180. So I'm going to process it to make sure.
         double translatedAngle = 0;
         // process the given angle to make sure it is in the range the user says it is
-        // if the angle is > 360, put it back in the range it is supposed to be in (0 to +360)
-        if (angle > 360) {
-            angle = angle % 360;
-        }
-        // if the angle is < 0 (negative) put it in the negative (0 - 360) range and
-        // then translate that to a positive angle so it ends up in the (0 - +360)
-        if (angle < 0) {
-            angle = angle % 360 + 360;
+        // if the angle is > 180, put it back in the range it is supposed to be in (-180 to +180)
+        // by dividing by 180 and using the remainder (modulo)
+        translatedAngle = angle % 180;
+        // if the angle is < 0 (negative) translate it to a positive angle so it ends up in the
+        // (0 to +360)
+        if (translatedAngle < 0) {
+            translatedAngle += 360;
         }
 
         // now that the angle is actually in the 0 - +360 range, translate it to one of these
         // cases
         switch (this) {
             case ZERO_TO_PLUS_360:
-                // the angle is already 0 to +360, just return it.
-                translatedAngle = angle;
+                // the angle is already 0 to +360, don't do anything to translated angle.
                 break;
             case ZERO_TO_MINUS_360:
                 // the angle is 0 to +360, subtract 360 to get it into -360 to 0 range
-                translatedAngle = angle - 360;
+                translatedAngle -= 360;
                 break;
             case PLUS_TO_MINUS_180:
-                // the angle is 0 to +360,
-                if (angle >= 0 && angle <= 180) {
-                    translatedAngle = angle;
-                } else {
-                    translatedAngle = angle - 360;
+                // the angle is 0 to +360, for 0 to 180 the angle is the same
+                if (translatedAngle > 180 && translatedAngle <= 360) {
+                    // for 180 to 360, the equivalent angle is -180 to 0 so subtract 360
+                    translatedAngle -= 360;
+                }
+                break;
+        }
+        return translatedAngle;
+    }
+
+    /**
+     * Translate the given angle, which is in the range of 0 to -360, to the range in this enum.
+     *
+     * @param angle
+     * @return
+     */
+    public double fromZeroToMinus360(double angle) {
+        // I'm not going to trust that the angle passed to this method is actually in the range
+        // of 0 to 360. So I'm going to process it to make sure.
+        double translatedAngle = 0;
+        // process the given angle to make sure it is in the range the user says it is
+        // if the angle is > 360, put it back in the range it is supposed to be in (0 to +360)
+        // by dividing by 360 and using the remainder (modulo)
+        translatedAngle = angle % 360;
+        // if the angle is > 0 (positive) translate it to a negative angle so it ends up in the
+        // (0 to -360)
+        if (translatedAngle > 0) {
+            translatedAngle -= 360;
+        }
+
+        // now that the angle is actually in the 0 to -360 range, translate it to one of these
+        // cases
+        switch (this) {
+            case ZERO_TO_PLUS_360:
+                // to get to 0 to +360 from -360 to 0, add 360
+                translatedAngle += 360;
+                break;
+            case ZERO_TO_MINUS_360:
+                // the angle already is 0 to -360, don't do anything
+                break;
+            case PLUS_TO_MINUS_180:
+                // the angle is 0 to -360, to get to +180 to -180 use two steps
+                // for 0 to -180 the angle is the same
+                // for -180 to -360, the equivalent angle is +180 to 0 so add 360
+                if (translatedAngle < -180 && translatedAngle >= -360) {
+                    translatedAngle += 360;
                 }
                 break;
         }
