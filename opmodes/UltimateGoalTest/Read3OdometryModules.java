d56e5b55dec256412ba657f4effe519e3f6d60e8
tatyk <tatyklochko@gmail.com>
2020-12-29 20:08:55 -0500

    FIXED THE ODOMETRY

--- opmodes/UltimateGoalTest/Read3OdometryModules.java
+++ opmodes/UltimateGoalTest/Read3OdometryModules.java
@@ -10,6 +10,8 @@ import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.OdometryModule;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.OdometrySystem;
 
+import java.util.Locale;
+
 @TeleOp(name = "Test 3 odometry modules", group = "Diagnostics")
 //@Disabled
 public class Read3OdometryModules extends LinearOpMode {
@@ -28,7 +30,7 @@ public class Read3OdometryModules extends LinearOpMode {
     DcMotor8863 FrontLeft;
     DcMotor8863 BackLeft;
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
 system = new OdometrySystem(DistanceUnit.INCH, odometryModuleLeft, odometryModuleRight, odometryModuleBack);
         odometryModuleRight = new OdometryModule(1440, 3.8 * Math.PI, DistanceUnit.CM, "BackLeft", hardwareMap);
         odometryModuleBack = new OdometryModule(1440, 3.8 * Math.PI, DistanceUnit.CM, "BackRight", hardwareMap);
@@ -39,8 +41,8 @@ system = new OdometrySystem(DistanceUnit.INCH, odometryModuleLeft, odometryModul
         BackRight = new DcMotor8863("BackRight",hardwareMap,telemetry);
         FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         FrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        BackRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         system =new OdometrySystem(DistanceUnit.INCH, odometryModuleLeft, odometryModuleRight, odometryModuleBack);
         //Odometry System Calibration Init Complete
         system.initializeRobotGeometry(DistanceUnit.INCH, 1,1, DcMotorSimple.Direction.REVERSE, 1,1, DcMotorSimple.Direction.FORWARD, 1,1, DcMotorSimple.Direction.REVERSE);
@@ -61,14 +63,14 @@ system = new OdometrySystem(DistanceUnit.INCH, odometryModuleLeft, odometryModul
             odometryModuleBackValue = odometryModuleBack.getEncoderValue();
             odometryModuleLeftValue = odometryModuleLeft.getEncoderValue();
             telemetry.addData("Odometry encoder Right = ", odometryModuleRightValue);
-            telemetry.addData("Odometry Encoder Right (In)", String.format("%.2f", odometryModuleRightValue / 1440 * 1.5 * Math.PI));
-            telemetry.addData("Odometry Encoder Right (cm)", String.format("%.2f", odometryModuleRight.getDistanceSinceReset(DistanceUnit.CM)));
+            telemetry.addData("Odometry Encoder Right (In)", String.format(Locale.getDefault(), "%.2f", odometryModuleRightValue / 1440 * 1.5 * Math.PI));
+            telemetry.addData("Odometry Encoder Right (cm)", String.format(Locale.getDefault(), "%.2f", odometryModuleRight.getDistanceSinceReset(DistanceUnit.CM)));
             telemetry.addData("Odometry encoder Back = ", odometryModuleBackValue);
-            telemetry.addData("Odometry Encoder Back (In)", String.format("%.2f", odometryModuleBackValue / 1440 * 1.5 * Math.PI));
-            telemetry.addData("Odometry Encoder Back (cm)", String.format("%.2f", odometryModuleBack.getDistanceSinceReset(DistanceUnit.CM)));
+            telemetry.addData("Odometry Encoder Back (In)", String.format(Locale.getDefault(), "%.2f", odometryModuleBackValue / 1440 * 1.5 * Math.PI));
+            telemetry.addData("Odometry Encoder Back (cm)", String.format(Locale.getDefault(), "%.2f", odometryModuleBack.getDistanceSinceReset(DistanceUnit.CM)));
             telemetry.addData("Odometry encoder Left = ", odometryModuleLeftValue);
-            telemetry.addData("Odometry Encoder Left (In)", String.format("%.2f", odometryModuleLeftValue / 1440 * 1.5 * Math.PI));
-            telemetry.addData("Odometry Encoder Left (cm)", String.format("%.2f", odometryModuleLeft.getDistanceSinceReset(DistanceUnit.CM)));
+            telemetry.addData("Odometry Encoder Left (In)", String.format(Locale.getDefault(), "%.2f", odometryModuleLeftValue / 1440 * 1.5 * Math.PI));
+            telemetry.addData("Odometry Encoder Left (cm)", String.format(Locale.getDefault(), "%.2f", odometryModuleLeft.getDistanceSinceReset(DistanceUnit.CM)));
             //Update values
             telemetry.addData("system says X is: ", system.getCurrentX(DistanceUnit.CM));
             telemetry.addData("system says y is: ", system.getCurrentY(DistanceUnit.CM));
