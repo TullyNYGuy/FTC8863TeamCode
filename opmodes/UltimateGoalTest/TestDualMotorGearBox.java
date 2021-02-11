d011b0b7a461f8ee9ced9048757372442ee9d231
Aidan Brown <aidanarisbrown@gmail.com>
2021-02-10 19:58:37 -0500

    Created a test for the dual motor gear box.

--- opmodes/UltimateGoalTest/TestDualMotorGearBox.java
+++ opmodes/UltimateGoalTest/TestDualMotorGearBox.java
@@ -1,8 +1,11 @@
-package opmodes.UltimateGoalTest;
+package org.firstinspires.ftc.teamcode.opmodes.UltimateGoalTest;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import Lib.UltimateGoalLib.DualMotorGearBox;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
@@ -12,12 +15,18 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class TestDualMotorGearBox extends LinearOpMode {
 
     // Put your variable declarations here
+    public DualMotorGearBox dualMotorGearBox;
+    public ElapsedTime timer;
+    public double speed;
 
     @Override
     public void runOpMode() {
 
 
         // Put your initializations here
+        dualMotorGearBox = new DualMotorGearBox("LeftMotor", "RightMotor", hardwareMap, telemetry);
+        timer = new ElapsedTime();
+        speed = 0.1;
 
         // Wait for the start button
         telemetry.addData(">", "Press Start to run");
@@ -25,12 +34,14 @@ public class TestDualMotorGearBox extends LinearOpMode {
         waitForStart();
 
         // Put your calls here - they will not run in a loop
-
-        while (opModeIsActive()) {
+        dualMotorGearBox.setSpeed(speed);
+        timer.reset();
+        while (opModeIsActive() && timer.milliseconds() < 5000) {
 
             // Put your calls that need to run in a loop here
 
             // Display the current value
+            telemetry.addData("Gear box running at ", speed * 100 + "%");
             //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
             //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
             telemetry.addData(">", "Press Stop to end test.");
@@ -39,8 +50,40 @@ public class TestDualMotorGearBox extends LinearOpMode {
 
             idle();
         }
+        dualMotorGearBox.stopGearbox();
 
+        while (opModeIsActive() && timer.milliseconds() < 5000) {
+
+            // Put your calls that need to run in a loop here
+
+            // Display the current value
+            telemetry.addData("Gear box stopping", "...");
+            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
+            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
+            telemetry.addData(">", "Press Stop to end test.");
+
+            telemetry.update();
+
+            idle();
+        }
+        dualMotorGearBox.setDirection(DualMotorGearBox.Direction.REVERSE);
+        dualMotorGearBox.setSpeed(speed);
+        while (opModeIsActive() && timer.milliseconds() < 5000) {
+
+            // Put your calls that need to run in a loop here
+
+            // Display the current value
+            telemetry.addData("Gear box running at ", speed * 100 + "%");
+            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
+            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
+            telemetry.addData(">", "Press Stop to end test.");
+
+            telemetry.update();
+
+            idle();
+        }
         // Put your cleanup code here - it runs as the application shuts down
+        dualMotorGearBox.stopGearbox();
         telemetry.addData(">", "Done");
         telemetry.update();
 
