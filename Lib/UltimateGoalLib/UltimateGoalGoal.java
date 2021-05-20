2af522f6a4e99534048dc6df743fd70be6d35110
TullyNYGuy <glenn_ball@utilitek.com>
2021-05-20 08:32:27 -0400

    created field and goal objects

--- Lib/UltimateGoalLib/UltimateGoalGoal.java
+++ Lib/UltimateGoalLib/UltimateGoalGoal.java
@@ -3,25 +3,76 @@ package org.firstinspires.ftc.teamcode.Lib.UltimateGoalLib;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-//         top = new UltimateGoalGoal(35.5 * 0.0254, new Pose2d(71 * 0.0254, -12 * 0.0254, 0));
-//         powerShotLeft = new UltimateGoalGoal(30 * 0.0254, new Pose2d(71 * 0.0254, 20 * 0.0254, 0));
-//         powerShotMid = new UltimateGoalGoal(30 * 0.0254, new Pose2d(71 * 0.0254, 12 * 0.0254, 0));
-//         powerShotRight = new UltimateGoalGoal(30 * 0.0254, new Pose2d(71 * 0.0254, 4.25 * 0.0254, 0));
-//         middle = new UltimateGoalGoal(26 * 0.0254, new Pose2d(71 * 0.0254, -12 * 0.0254, 0));
-//         bottom = new UltimateGoalGoal(16.5 * 0.0254, new Pose2d(71 * 0.0254, -12 * 0.0254, 0));
 
-public interface UltimateGoalGoal {
+public class UltimateGoalGoal {
+    //*********************************************************************************************
+    //          ENUMERATED TYPES
+    //
+    // user defined types
+    //
+    //*********************************************************************************************
 
-    /**
-     * Returns the goal height in meters.
-     * @return
-     */
-    public double getHeight();
+    //*********************************************************************************************
+    //          PRIVATE DATA FIELDS
+    //
+    // can be accessed only by this class, or by using the public
+    // getter and setter methods
+    //*********************************************************************************************
+    private double height = 0;
+    private Pose2d pose2d;
+    private DistanceUnit units;
 
-    /**
-     * returns the goal position in (x (inches), y (inches), heading (radians))
-     * @return
-     */
-    public Pose2d getPosition();
+    //*********************************************************************************************
+    //          GETTER and SETTER Methods
+    //
+    // allow access to private data fields for example setMotorPower,
+    // getPositionInTermsOfAttachment
+    //*********************************************************************************************
+
+    public double getHeight(DistanceUnit desiredUnits) {
+        return desiredUnits.fromUnit(this.units, height);
+    }
+
+    public double getX(DistanceUnit desiredUnits) {
+        return desiredUnits.fromUnit(this.units, pose2d.getX());
+    }
+
+    public double getY(DistanceUnit desiredUnits) {
+        return desiredUnits.fromUnit(this.units, pose2d.getY());
+    }
+    public Pose2d getPose2d() {
+        return pose2d;
+    }
+
+    public DistanceUnit getUnits() {
+        return units;
+    }
+
+    //*********************************************************************************************
+    //          Constructors
+    //
+    // the function that builds the class when an object is created
+    // from it
+    //*********************************************************************************************
+    public UltimateGoalGoal(DistanceUnit units, double height, Pose2d pose2d) {
+        // a pose2D is in units of inches so we will store the data in inches
+        this.units = DistanceUnit.INCH;
+        this.height = this.units.fromUnit(units, height);
+        this.pose2d = pose2d;
+    }
+
+    //*********************************************************************************************
+    //          Helper Methods
+    //
+    // methods that aid or support the major functions in the class
+    //*********************************************************************************************
+
+    //*********************************************************************************************
+    //          MAJOR METHODS
+    //
+    // public methods that give the class its functionality
+    //*********************************************************************************************
+
 }
