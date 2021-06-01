76e9273f32f2cf941c50d59c44923b5f3ffefb3f
tatyk <tatyklochko@gmail.com>
2021-06-01 19:02:23 -0400

    add angle units to set shooter angle

--- Lib/UltimateGoalLib/PersistantStorage.java
+++ Lib/UltimateGoalLib/PersistantStorage.java
@@ -5,15 +5,26 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
+import java.security.interfaces.DSAPublicKey;
+
 public class PersistantStorage {
 
     // public static DATA FIELDS that persist between opmodes
 
     // angle changer object
 
-    //public static AngleChanger angleChanger;
-    public static AngleUnit angleUnit = AngleUnit.RADIANS;
-    public static Double shooterAngle = null;
+    public static double getShooterAngle(AngleUnit inputUnits) {
+        if (shooterAngle==null){
+            shooterAngle=new Double(0);
 
-    public static Pose2d robotPose = null;
+        }
+        return shooterAngle;
+    }
+
+    public static void setShooterAngle(double shooterAngle, AngleUnit units) {
+        PersistantStorage.shooterAngle = AngleUnit.RADIANS.fromUnit(units, shooterAngle);
+    }
+
+    private static Double shooterAngle;
+    public static Pose2d robotPose;
 }
