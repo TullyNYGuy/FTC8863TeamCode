3063b0a2ece2ceb8bc0fd1a125e4cd04ed619f1a
TullyNYGuy <glenn_ball@utilitek.com>
2021-12-04 15:39:21 -0500

    Lift test opmodes

--- opmodes/FreightFrenzyTest/TestLiftReset.java
+++ opmodes/FreightFrenzyTest/TestLiftReset.java
@@ -1,44 +1,50 @@
 package org.firstinspires.ftc.teamcode.opmodes.FreightFrenzyTest;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.ExtensionRetractionMechanism;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
  */
-@TeleOp(name = "Lift Test", group = "Test")
+@TeleOp(name = "Test Lift Reset", group = "Test")
 //@Disabled
-public class TestLift extends LinearOpMode {
+public class TestLiftReset extends LinearOpMode {
 
-    // Put your variable declarations her
+    // Put your variable declarations here
+    ExtensionRetractionMechanism lift;
 
     @Override
     public void runOpMode() {
 
 
         // Put your initializations here
+        lift = new ExtensionRetractionMechanism(hardwareMap,
+                telemetry,
+                "lift",
+                "ExtensionLimitSwitch",
+                "RetractionLimitSwitch",
+                "LiftMotor",
+                DcMotor8863.MotorType.ANDYMARK_20_ORBITAL,
+                3.926);
+
+      //  lift.reverseMotorDirection();
+
 
         // Wait for the start button
-        telemetry.addData("I'm Tired", "!");
         telemetry.addData(">", "Press Start to run");
         telemetry.update();
         waitForStart();
 
         // Put your calls here - they will not run in a loop
 
+        lift.testReset(this);
+
         while (opModeIsActive()) {
-
-            // Put your calls that need to run in a loop here
-
-            // Display the current value
-            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
-            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
-            telemetry.addData("Hi Melody","!");
-            telemetry.addData(">", "Press Stop to end test.");
-
+            telemetry.addData("Reset Complete", "!");
             telemetry.update();
-
             idle();
         }
 
