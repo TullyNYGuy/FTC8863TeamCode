81d12ef7bf339c92969f7c3ed15b12201726dcaa
TullyNYGuy <glenn_ball@utilitek.com>
2022-01-28 17:20:03 -0500

    Manually updated some of list tests

--- opmodes/FreightFrenzyTest/TestLiftExtension.java
+++ opmodes/FreightFrenzyTest/TestLiftExtension.java
@@ -1,27 +1,32 @@
 package org.firstinspires.ftc.teamcode.opmodes.FreightFrenzyTest;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import android.provider.ContactsContract;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DcMotor8863;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.ExtensionRetractionMechanism;
 
 /**
  * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
  */
-@TeleOp(name = "Test Lift Switches", group = "Test")
+@TeleOp(name = "Test Lift Extension", group = "Test")
 //@Disabled
-public class TestLiftLimitSwitches extends LinearOpMode {
+public class TestLiftExtension extends LinearOpMode {
 
     // Put your variable declarations here
     ExtensionRetractionMechanism lift;
+    DataLogging log;
 
     @Override
     public void runOpMode() {
 
 
         // Put your initializations here
+
+        log = new DataLogging("LiftLog");
         lift = new ExtensionRetractionMechanism(hardwareMap, telemetry,
                 "Lift",
                 "ExtensionLimitSwitch",
@@ -31,6 +36,14 @@ public class TestLiftLimitSwitches extends LinearOpMode {
                 4.517);
         lift.reverseMotorDirection();
 
+
+        //lift.reverseMotorDirection();
+        lift.setResetTimerLimitInmSec(5000);
+        lift.setExtensionPower(0.2);
+        lift.setExtensionPositionInMechanismUnits(10.0);
+        lift.setDataLog(log);
+        lift.enableDataLogging();
+
         // Wait for the start button
         telemetry.addData(">", "Press Start to run");
         telemetry.update();
@@ -38,14 +51,12 @@ public class TestLiftLimitSwitches extends LinearOpMode {
 
         // Put your calls here - they will not run in a loop
 
-        while (opModeIsActive()) {
+        lift.testExtension(this);
 
-            // Put your calls that need to run in a loop here
-
-            lift.testLimitSwitches();
-            telemetry.addData(">", "Press Stop to end test.");
+        // after the extension is complete, loop so the user can see the result
+        while (opModeIsActive()){
+            telemetry.addData("state = ", lift.getExtensionRetractionState().toString());
             telemetry.update();
-
             idle();
         }
 
@@ -55,3 +66,4 @@ public class TestLiftLimitSwitches extends LinearOpMode {
 
     }
 }
+
