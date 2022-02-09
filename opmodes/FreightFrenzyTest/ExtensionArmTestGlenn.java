a65e61e349afc09dc468342af137a157a97a4632
TullyNYGuy <glenn_ball@utilitek.com>
2022-02-08 21:08:29 -0500

    Added extension arm tests from glenn

---
+++ opmodes/FreightFrenzyTest/ExtensionArmTestGlenn.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.opmodes.FreightFrenzyTest;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.FFExtensionArm;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.FFExtensionArmGlenn;
+
+/**
+ * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
+ */
+@TeleOp(name = "Extension Arm test Glenn ", group = "Test")
+//@Disabled
+public class ExtensionArmTestGlenn extends LinearOpMode {
+
+    // Put your variable declarations her
+    private FFExtensionArmGlenn delivery;
+    @Override
+    public void runOpMode() {
+        delivery = new FFExtensionArmGlenn(hardwareMap, telemetry);
+
+        // Put your initializations here
+        delivery.init();
+        while(!delivery.isInitComplete()){
+            idle();
+        }
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to run");
+        telemetry.update();
+        waitForStart();
+
+        // Put your calls here - they will not run in a loop
+            delivery.extend();
+        while (opModeIsActive()) {
+
+            // Put your calls that need to run in a loop here
+            delivery.update();
+            telemetry.addData("Super Cool State:", delivery.getLiftState());
+            // Display the current value
+            //telemetry.addData("Motor Speed = ", "%5.2f", powerToRunAt);
+            //telemetry.addData("Encoder Count=", "%5d", motor.getCurrentPosition());
+            telemetry.addData(">", "Press Stop to end test.");
+
+            telemetry.update();
+
+            idle();
+        }
+
+        // Put your cleanup code here - it runs as the application shuts down
+        telemetry.addData(">", "Done");
+        telemetry.update();
+
+    }
+}
