ab2f13711fc60124fa9d15ad4f0db62a36325475
TullyNYGuy <glenn_ball@utilitek.com>
2022-02-26 09:42:22 -0500

    New test for verifying delivery system function in Autonomous

---
+++ Lib/FreightFrenzyLib/AutonomousTestDeliveryStateMachine.java
@@ -0,0 +1,285 @@
+package org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.Pose2d8863;
+
+public class AutonomousDuckSpinVisionLoadParkStorage implements AutonomousStateMachineFreightFrenzy {
+
+    //*********************************************************************************************
+    //          ENUMERATED TYPES
+    //
+    // user defined types
+    //
+    //*********************************************************************************************
+    public enum States {
+        IDLE,
+        START,
+        MOVING_TO_DUCKS,
+        DUCK_SPINNING,
+        MOVING_TO_WAYPOINT_BEFORE_HUB,
+        WAITING_TO_EXTEND,
+        MOVING_TO_HUB,
+        EXTENDING_LIFT,
+        DUMPING,
+        MOVING_TO_WAYPOINT_BEFORE_PARK,
+        MOVING_TO_PARK,
+        COMPLETE
+    }
+
+    //*********************************************************************************************
+    //          PRIVATE DATA FIELDS
+    //
+    // can be accessed only by this class, or by using the public
+    // getter and setter methods
+    //*********************************************************************************************
+    private Pose2d START_POSE;
+    private States currentState;
+    private FreightFrenzyRobotRoadRunner robot;
+    private FreightFrenzyField field;
+    private ElapsedTime timer;
+    private DistanceUnit distanceUnits;
+    private AngleUnit angleUnits;
+    private Pose2d hubDumpPose;
+    private Trajectory trajectoryToHub;
+    private Trajectory trajectoryToDucks;
+    private Trajectory trajectoryToStorage;
+    private Trajectory trajectoryToWaypoint;
+    private Trajectory trajectoryToWaypointBeforePark;
+    private double distanceToTopGoal = 0;
+    private double distanceToLeftPowerShot = 0;
+    private double angleOfShot = 0;
+    private boolean isComplete = false;
+
+    //*********************************************************************************************
+    //          GETTER and SETTER Methods
+    //
+    // allow access to private data fields for example setMotorPower,
+    // getPositionInTermsOfAttachment
+    //*********************************************************************************************
+
+    @Override
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    @Override
+    public String getCurrentState(){
+        return currentState.toString();
+    }
+
+    //*********************************************************************************************
+    //          Constructors
+    //
+    // the function that builds the class when an object is created
+    // from it
+    //*********************************************************************************************
+
+    public AutonomousDuckSpinVisionLoadParkStorage(FreightFrenzyRobotRoadRunner robot, FreightFrenzyField field, Telemetry telemetry) {
+        this.robot = robot;
+        this.field = field;
+        switch (PersistantStorage.getShippingElementPosition()) {
+            case CENTER:
+                robot.freightSystem.setMiddle();
+                switch (PersistantStorage.getStartSpot()) {
+                    case BLUE_WALL:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_MID_BLUE_WALL;
+                        break;
+                    case RED_WALL:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_MID_RED_WALL;
+                        break;
+                    case BLUE_WAREHOUSE:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_MID_BLUE_WAREHOUSE;
+                        break;
+                    case RED_WAREHOUSE:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_MID_RED_WAREHOUSE;
+                        break;
+                }
+                break;
+            case LEFT:
+                robot.freightSystem.setBottom();
+                switch (PersistantStorage.getStartSpot()) {
+                    case BLUE_WALL:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_LOW_BLUE_WALL;
+                        break;
+                    case RED_WALL:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_LOW_RED_WALL;
+                        break;
+                    case BLUE_WAREHOUSE:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_LOW_BLUE_WAREHOUSE;
+                        break;
+                    case RED_WAREHOUSE:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_LOW_RED_WAREHOUSE;
+                        break;
+                }
+                break;
+            case RIGHT:
+                robot.freightSystem.setTop();
+                switch (PersistantStorage.getStartSpot()) {
+                    case BLUE_WALL:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_HIGH_HUB_BLUE_WALL;
+                        break;
+                    case RED_WALL:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_HIGH_HUB_RED_WALL;
+                        break;
+                    case BLUE_WAREHOUSE:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_HIGH_HUB_BLUE_WAREHOUSE;
+                        break;
+                    case RED_WAREHOUSE:
+                        hubDumpPose = PoseStorageFF.DELIVER_TO_HIGH_HUB_RED_WAREHOUSE;
+                        break;
+                }
+                break;
+        }
+        currentState = States.IDLE;
+        distanceUnits = DistanceUnit.INCH;
+        angleUnits = AngleUnit.DEGREES;
+        timer = new ElapsedTime();
+        START_POSE = PersistantStorage.getStartPosition();
+        PoseStorageFF.retreiveStartPose();
+        createTrajectories();
+    }
+
+    //*********************************************************************************************
+    //          Helper Methods
+    //
+    // methods that aid or support the major functions in the class
+    //*********************************************************************************************
+
+    /**
+     * Place all of the trajectories for the autonomous opmode in this method. This method gets
+     * called from the constructor so that the trajectories are created when the autonomous object
+     * is created.
+     */
+    @Override
+    public void createTrajectories() {
+
+
+        if (PersistantStorage.getAllianceColor() == AllianceColor.BLUE) {
+            trajectoryToDucks = robot.mecanum.trajectoryBuilder(PoseStorageFF.START_POSE)
+                    .lineToLinearHeading(PoseStorageFF.DUCK_SPINNER_BLUE)
+                    .build();
+            trajectoryToWaypoint= robot.mecanum.trajectoryBuilder(trajectoryToDucks.end())
+                    .lineToLinearHeading(PoseStorageFF.WAYPOINT_BLUE_HUB)
+                    .build();
+            trajectoryToHub = robot.mecanum.trajectoryBuilder(trajectoryToWaypoint.end())
+                    .lineToLinearHeading(hubDumpPose)
+                    .build();
+            trajectoryToWaypointBeforePark = robot.mecanum.trajectoryBuilder(trajectoryToHub.end())
+                    .lineToLinearHeading(PoseStorageFF.WAYPOINT_BLUE_HUB)
+                    .build();
+            trajectoryToStorage = robot.mecanum.trajectoryBuilder(trajectoryToWaypointBeforePark.end())
+                    .lineToLinearHeading(PoseStorageFF.STORAGE_BLUE)
+                    .build();
+
+
+        } else {
+            trajectoryToDucks = robot.mecanum.trajectoryBuilder(PoseStorageFF.START_POSE)
+                    .lineToLinearHeading(PoseStorageFF.DUCK_SPINNER_RED)
+                    .build();
+            trajectoryToWaypoint= robot.mecanum.trajectoryBuilder(trajectoryToDucks.end())
+                    .lineToLinearHeading(PoseStorageFF.WAYPOINT_RED_HUB)
+                    .build();
+            trajectoryToHub = robot.mecanum.trajectoryBuilder(trajectoryToWaypoint.end())
+                    .lineToLinearHeading(hubDumpPose)
+                    .build();
+            trajectoryToWaypointBeforePark = robot.mecanum.trajectoryBuilder(trajectoryToHub.end())
+                    .lineToLinearHeading(PoseStorageFF.WAYPOINT_RED_HUB)
+                    .build();
+            trajectoryToStorage = robot.mecanum.trajectoryBuilder(trajectoryToWaypointBeforePark.end())
+                    .lineToLinearHeading(PoseStorageFF.STORAGE_RED)
+                    .build();
+
+        }
+    }
+
+        //*********************************************************************************************
+        //          MAJOR METHODS
+        //
+        // public methods that give the class its functionality
+        //*********************************************************************************************
+
+        @Override
+        public void start () {
+            currentState = States.START;
+            isComplete = false;
+        }
+
+        @Override
+        public void update () {
+            switch (currentState) {
+                case START:
+                    isComplete = false;
+                    robot.mecanum.setPoseEstimate(PoseStorageFF.START_POSE);
+                    robot.mecanum.followTrajectory(trajectoryToDucks);
+                    currentState = States.MOVING_TO_DUCKS;
+                    break;
+
+                case MOVING_TO_DUCKS:
+                    if (!robot.mecanum.isBusy()) {
+                        robot.duckSpinner.turnOn();
+                        currentState = States.DUCK_SPINNING;
+                    }
+                    break;
+
+                case DUCK_SPINNING:
+                    if (robot.duckSpinner.isComplete()) {
+                        robot.mecanum.followTrajectory(trajectoryToWaypoint);
+                        currentState = States.MOVING_TO_WAYPOINT_BEFORE_HUB;
+                    }
+                    break;
+
+                case MOVING_TO_WAYPOINT_BEFORE_HUB:
+                    if(!robot.mecanum.isBusy()) {
+                        robot.freightSystem.extend();
+                        currentState = States.WAITING_TO_EXTEND;
+                    }
+                    break;
+
+                case WAITING_TO_EXTEND:
+                    if (robot.freightSystem.isReadyToDump()) {
+                        robot.mecanum.followTrajectory(trajectoryToHub);
+                        currentState = States.MOVING_TO_HUB;
+                    }
+                    break;
+
+                case MOVING_TO_HUB:
+                    if (!robot.mecanum.isBusy()) {
+                        robot.freightSystem.dump();
+                        currentState = States.DUMPING;
+                    }
+                    break;
+
+                case DUMPING:
+                    if (robot.freightSystem.isDumpComplete()) {
+                        robot.mecanum.followTrajectory(trajectoryToWaypointBeforePark);
+                        currentState = States.MOVING_TO_WAYPOINT_BEFORE_PARK;
+                    }
+                    break;
+
+                case MOVING_TO_WAYPOINT_BEFORE_PARK:
+                    if(!robot.mecanum.isBusy()) {
+                        robot.mecanum.followTrajectory(trajectoryToStorage);
+                        currentState = States.MOVING_TO_PARK;
+                    }
+
+                    break;
+                case MOVING_TO_PARK:
+                    if (!robot.mecanum.isBusy()) {
+                        currentState = States.COMPLETE;
+                    }
+
+                    break;
+                    
+                case COMPLETE:
+                    isComplete = true;
+                    break;
+            }
+        }
+    }
---
+++ opmodes/FreightFrenzy/AutonomousTestDelivery.java
@@ -0,0 +1,194 @@
+package org.firstinspires.ftc.teamcode.opmodes.FreightFrenzy;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.Configuration;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.AutonomousDuckSpinVisionLoadParkDepot;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.AutonomousDuckSpinVisionLoadParkStorage;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.AutonomousVisionLoadDuckSpinParkDepot;
+
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.AutonomousStateMachineFreightFrenzy;
+
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.FreightFrenzyField;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.FreightFrenzyGamepad;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.FreightFrenzyMatchInfo;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.FreightFrenzyRobotRoadRunner;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.FreightFrenzyStartSpot;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.PersistantStorage;
+import org.firstinspires.ftc.teamcode.Lib.FreightFrenzyLib.Pipelines.ShippingElementPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.List;
+
+/**
+ * This Opmode is a shell for a linear OpMode. Copy this file and fill in your code as indicated.
+ */
+@Autonomous(name = "Autonomous Freight Frenzy", group = "AA")
+//@Disabled
+public class AutonomousWithVisionFreightFrenzy extends LinearOpMode {
+
+    // Put your variable declarations her
+    public FreightFrenzyRobotRoadRunner robot;
+    public FreightFrenzyGamepad gamepad;
+    public Configuration config;
+    public FreightFrenzyStartSpot startSpot;
+    private ElapsedTime timer;
+    public boolean autoDone;
+    private ShippingElementPipeline.ShippingPosition position;
+    DataLogging dataLog = null;
+    int cameraMonitorViewId;
+    public FreightFrenzyField field;
+    public double distance = 0;
+    public double angleBetween = 0;
+    private AutonomousStateMachineFreightFrenzy autonomous;
+    private ShippingElementPipeline pipeline;
+
+    @Override
+    public void runOpMode() {
+        FreightFrenzyMatchInfo.setMatchPhase(FreightFrenzyMatchInfo.MatchPhase.AUTONOMOUS);
+        pipeline = new ShippingElementPipeline();
+        telemetry.addData("Initializing ...", "Wait for it ...");
+        telemetry.update();
+        dataLog = new DataLogging("Autonomous", telemetry);
+        config = null;
+        //cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        timer = new ElapsedTime();
+        field = new FreightFrenzyField();
+        robot = new FreightFrenzyRobotRoadRunner(hardwareMap, telemetry, config, dataLog, DistanceUnit.CM, this);
+        robot.createRobot();
+        startSpot = PersistantStorage.getStartSpot();
+
+        //int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        //robot.activeWebcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, robot.getCameraName()), cameraMonitorViewId);
+        //robot.activeWebcam .setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        robot.activeWebcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                robot.activeWebcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+        robot.activeWebcam.setPipeline(pipeline);
+        timer.reset();
+        while (pipeline.getAnalysis() == ShippingElementPipeline.ShippingPosition.UNKNOWN) {
+            // TANYA - need the idle so we don't hog all the CPU
+            //pipeline.getAnalysis();
+            idle();
+            //autonomous.update();
+        }
+
+        position = pipeline.getAnalysis();
+        PersistantStorage.setShippingElementPosition(position);
+        telemetry.addData("shipping element postion is", position);
+        telemetry.update();
+       /* switch(startSpot){
+            case BLUE_WALL:
+                cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                robot.webcamLeft.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+                @Override
+                public void onOpened() {
+                    robot.webcamLeft.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                }
+
+                @Override
+                public void onError(int errorCode) {
+
+                }
+            });
+
+                break;
+            case RED_WALL:
+                cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                robot.webcamRight.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+                @Override
+                public void onOpened() {
+                    robot.webcamLeft.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                }
+
+                @Override
+                public void onError(int errorCode) {
+
+                }
+            });
+                break;
+            case BLUE_WAREHOUSE:
+                cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                robot.webcamLeft.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+                @Override
+                public void onOpened() {
+                    robot.webcamLeft.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                }
+
+                @Override
+                public void onError(int errorCode) {
+
+                }
+            });
+                break;
+            case RED_WAREHOUSE:
+                cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+                robot.webcamRight.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+                @Override
+                public void onOpened() {
+                    robot.webcamLeft.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                }
+
+                @Override
+                public void onError(int errorCode) {
+
+                }
+            });
+                break;
+        }*/
+
+        enableBulkReads(hardwareMap, LynxModule.BulkCachingMode.AUTO);
+        autonomous = new AutonomousDuckSpinVisionLoadParkStorage(robot, field, telemetry);
+
+        timer.reset();
+        robot.loopTimer.startLoopTimer();
+
+
+        telemetry.addData(">", "Press Start to run");
+        telemetry.update();
+        telemetry.addData("shipping element postion is", position);
+        telemetry.update();
+        robot.freightSystem.setPhaseAutonomus();
+
+        // Wait for the start button
+        waitForStart();
+        robot.activeWebcam.closeCameraDevice();
+
+        autonomous.start();
+        while (opModeIsActive() && !autonomous.isComplete()) {
+            autonomous.update();
+            telemetry.addData("current state is", autonomous.getCurrentState());
+            telemetry.update();
+            robot.update();
+            idle();
+        }
+
+        robot.shutdown();
+        dataLog.closeDataLog();
+        telemetry.addData(">", "Done");
+        telemetry.update();
+    }
+
+    public void enableBulkReads(HardwareMap hardwareMap, LynxModule.BulkCachingMode mode) {
+        // set bulk read mode for the sensor reads - speeds up the loop
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(mode);
+        }
+    }
+}
