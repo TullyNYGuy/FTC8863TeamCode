5d022baabf0c22ff8973501b94c00f4e14327335
TullyNYGuy <glenn_ball@utilitek.com>
2023-01-04 17:35:42 -0500

    Added persistent storage to be used between auto and teleop.

---
+++ Lib/PowerPlayLib/PowerPlayPersistantStorage.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Lib.PowerPlayLib;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class PowerPlayPersistantStorage {
+
+    // public static DATA FIELDS that persist between opmodes
+
+    public static Pose2d robotPose;
+
+}
