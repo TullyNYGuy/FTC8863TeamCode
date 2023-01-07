a8e709f6f3fe0f56f6f79350f2812781300871f0
TullyNYGuy <glenn_ball@utilitek.com>
2023-01-07 08:03:07 -0500

    Interface for Autonomous

---
+++ Lib/PowerPlayLib/PowerPlayAutonomousStateMachine.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Lib.PowerPlayLib;
+
+
+public interface PowerPlayAutonomousStateMachine {
+
+    /**
+     * This method will be called when you want to start the autonomous. Typically, your class will
+     * have some variable that saves whether the autonomous is complete or not. So your start
+     * method will probably set that variable to indicate that the autonomous is not complete. If
+     * your class uses a state machine to sequence your auto, then this method should start the
+     * state machine
+     */
+     void start();
+
+    /**
+     * This method will contain statements that create all of the road runner trajectories for your
+     * auto. This method should be called from the constructor for your class. Each trajectory takes
+     * about 1/2 second to generate so generating them all before you actually run the auto saves
+     * time.
+     */
+     void createTrajectories();
+
+    /**
+     * This method will typically run the state machine for your auto.
+     */
+     void update();
+
+    /**
+     * This method will tell the user if your auto is finished or not.
+     * @return - true if complete, false if still running
+     */
+     boolean isComplete();
+
+     String getCurrentState();
+}
