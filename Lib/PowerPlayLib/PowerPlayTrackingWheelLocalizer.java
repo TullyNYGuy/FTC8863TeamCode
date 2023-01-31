a7e7fbc7c9df3e7b9ba9e08eeb87b00c6ccf9147
TullyNYGuy <glenn_ball@utilitek.com>
2023-01-31 17:55:01 -0500

    Adjust lateral encoder distance.

--- Lib/PowerPlayLib/PowerPlayTrackingWheelLocalizer.java
+++ Lib/PowerPlayLib/PowerPlayTrackingWheelLocalizer.java
@@ -34,7 +34,8 @@ public class PowerPlayTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer
     public static double WHEEL_RADIUS = .748; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 10.7969; // in; distance between the left and right wheels
+    //public static double LATERAL_DISTANCE = 10.7969; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 10.57; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -5.5; // in; offset of the lateral wheel
 
     /**
