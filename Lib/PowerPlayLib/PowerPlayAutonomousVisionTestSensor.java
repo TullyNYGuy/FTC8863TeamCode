a9fea23e0188ac087768081bade7eb97f88c45ea
TullyNYGuy <glenn_ball@utilitek.com>
2023-02-10 16:30:52 -0500

    Auto to test the sensors in auto.

---
+++ Lib/PowerPlayLib/PowerPlayAutonomousVisionTestSensor.java
@@ -0,0 +1,342 @@
+package org.firstinspires.ftc.teamcode.Lib.PowerPlayLib;
+
+
+import static org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayField.getVector2d;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.AllianceColorTeamLocation;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogOnChange;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
+
+public class PowerPlayAutonomousVisionTestSensor implements PowerPlayAutonomousStateMachine {
+
+    //*********************************************************************************************
+    //          ENUMERATED TYPES
+    //
+    // user defined types
+    //
+    //*********************************************************************************************
+    public enum States {
+        IDLE,
+        START,
+        MOVING_TO_JUNCTION_POLE_FOR_SCORE,
+        RAISING_LIFT,
+        DROPPING_FOUR_INCHES,
+        RELEASING_OPEN_LIFT,
+        MOVING_TO_PARKING,
+        COMPLETE,
+        READING_POLE_WITH_INVERSE_SENSOR,
+        READING_POLE_WITH_NORMAL_SENSOR
+    }
+
+    //*********************************************************************************************
+    //          PRIVATE DATA FIELDS
+    //
+    // can be accessed only by this class, or by using the public
+    // getter and setter methods
+    //*********************************************************************************************
+    private Pose2d startPose;
+    private Pose2d junctionPolePose;
+    private Pose2d parkingLocationPose;
+    private Trajectory trajectoryToJunctionPoleFromStart;
+    private Trajectory trajectoryToParkingLocation;
+    private Trajectory trajectoryToParkingLocation1;
+    private Trajectory trajectoryToParkingLocation2;
+    private Trajectory trajectoryToParkingLocation3;
+
+    private AllianceColorTeamLocation.ColorLocation colorLocation;
+
+    private PowerPlayRobot robot;
+    private PowerPlayField field;
+    private Telemetry telemetry;
+
+    private ElapsedTime timer;
+    private DistanceUnit distanceUnits;
+    private AngleUnit angleUnits;
+
+    private States currentState;
+    private boolean isComplete = false;
+
+    private DataLogging logFile;
+    private boolean enableLogging = false;
+    private DataLogOnChange logStateOnChange;
+    private DataLogOnChange logCommandOnchange;
+
+    private PowerPlayField.ParkLocation parkLocation;
+
+    /**
+     * This method is needed because the trajectories are calculated at the time the autonomous is created.
+     * This is before the signal cone is randomized. So the robot sits in init, looking at the cone the entire
+     * time. Finally once play is pressed the robot knows that the signal cone is set and it quickly reads the
+     * image (color in our case). We can finally determine which of the previoulsy calculated trajectories to
+     * the parking location is the one the robot actually has to use. This method is called after the final
+     * signal cone image is read and the parking location is determined. Basically call it just after play is
+     * pressed on the driver station.
+     * @param parkLocation
+     */
+    @Override
+    public void setParkLocation(PowerPlayField.ParkLocation parkLocation) {
+        this.parkLocation = parkLocation;
+
+        // set the actual trajectory to the parking location to one of the 3 previously calculated
+        // trajectories, depending on which one was determined from the signal cone
+        switch (parkLocation) {
+            case ONE: {
+                trajectoryToParkingLocation = trajectoryToParkingLocation1;
+            }
+            break;
+
+            case TWO: {
+                trajectoryToParkingLocation = trajectoryToParkingLocation2;
+            }
+            break;
+
+            case THREE: {
+                trajectoryToParkingLocation = trajectoryToParkingLocation3;
+            }
+            break;
+        }
+    }
+    //*********************************************************************************************
+    //          GETTER and SETTER Methods
+    //
+    // allow access to private data fields for example setMotorPower,
+    // getPositionInTermsOfAttachment
+    //*********************************************************************************************
+
+    @Override
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    @Override
+    public String getCurrentState() {
+        return currentState.toString();
+    }
+
+    //*********************************************************************************************
+    //          Constructors
+    //
+    // the function that builds the class when an object is created
+    // from it
+    //*********************************************************************************************
+
+    public PowerPlayAutonomousVisionTestSensor(PowerPlayRobot robot, PowerPlayField field, Telemetry telemetry) {
+        this.robot = robot;
+        this.field = field;
+        this.telemetry = telemetry;
+        this.colorLocation = PowerPlayPersistantStorage.getColorLocation();
+        startPose = field.getStartPose();
+        junctionPolePose = field.getJunctionPolePose();
+
+        currentState = States.IDLE;
+        distanceUnits = DistanceUnit.INCH;
+        angleUnits = AngleUnit.DEGREES;
+        timer = new ElapsedTime();
+
+        createTrajectories();
+    }
+
+    //*********************************************************************************************
+    //          Helper Methods
+    //
+    // methods that aid or support the major functions in the class
+    //*********************************************************************************************
+    @Override
+    public String getName() {
+        return "Auto";
+    }
+
+    @Override
+    public void setDataLog(DataLogging logFile) {
+        this.logFile = logFile;
+        logCommandOnchange = new DataLogOnChange(logFile);
+        logStateOnChange = new DataLogOnChange(logFile);
+    }
+
+    @Override
+    public void enableDataLogging() {
+        enableLogging = true;
+    }
+
+    @Override
+    public void disableDataLogging() {
+        enableLogging = false;
+    }
+
+    private void logState() {
+        if (enableLogging && logFile != null) {
+            logStateOnChange.log(getName() + " state = " + currentState.toString());
+        }
+    }
+
+    private void logCommand(String command) {
+        if (enableLogging && logFile != null) {
+            logCommandOnchange.log(getName() + " command = " + command);
+        }
+    }
+
+    /**
+     * Place all of the trajectories for the autonomous opmode in this method. This method gets
+     * called from the constructor so that the trajectories are created when the autonomous object
+     * is created.
+     */
+    @Override
+    public void createTrajectories() {
+        switch (PowerPlayPersistantStorage.getColorLocation()) {
+            case BLUE_LEFT:
+            case RED_LEFT: {
+                trajectoryToJunctionPoleFromStart = robot.mecanum.trajectoryBuilder(startPose)
+                        .splineTo(new Vector2d(-11.75, -53), Math.toRadians(90))
+                        .splineToSplineHeading(junctionPolePose,Math.toRadians(90))
+                        .build();
+
+                trajectoryToParkingLocation1 = null;
+
+                trajectoryToParkingLocation2 = robot.mecanum.trajectoryBuilder(trajectoryToJunctionPoleFromStart.end())
+                        .lineTo(new Vector2d(-11.75, -20.5))
+                        .splineToConstantHeading(new Vector2d(-23.5, -10.75), Math.toRadians(-180))
+                        .splineToConstantHeading(getVector2d(PowerPlayPoseStorageForPowerPlayDrive.RED_LEFT_PARK_LOCATION_2), Math.toRadians(0))
+                        .build();
+
+                trajectoryToParkingLocation3 = robot.mecanum.trajectoryBuilder(trajectoryToJunctionPoleFromStart.end())
+                        .lineTo(new Vector2d(-11.75, -20.5))
+                        .splineToConstantHeading(new Vector2d(-23.5, -10.75), Math.toRadians(180))
+                        .splineToConstantHeading(getVector2d(PowerPlayPoseStorageForPowerPlayDrive.RED_LEFT_PARK_LOCATION_3), Math.toRadians(180))
+                        .build();
+            }
+            break;
+
+            case BLUE_RIGHT:
+            case RED_RIGHT: {
+                trajectoryToJunctionPoleFromStart = robot.mecanum.trajectoryBuilder(startPose)
+                        .splineTo(new Vector2d(11.75, -53), Math.toRadians(90))
+                        .splineToSplineHeading(junctionPolePose,Math.toRadians(90))
+                        .build();
+
+                // don't need to move since the robot is already in the parking location when it scores
+                trajectoryToParkingLocation1 = null;
+//                trajectoryToParkingLocation1 = robot.mecanum.trajectoryBuilder(trajectoryToJunctionPoleFromStart.end())
+//                        .lineToLinearHeading(PowerPlayPoseStorage.RED_RIGHT_PARK_LOCATION_1)
+//                        .build();
+
+                trajectoryToParkingLocation2 = robot.mecanum.trajectoryBuilder(trajectoryToJunctionPoleFromStart.end())
+                        // end tangent forms a nice curve
+                        .splineToConstantHeading(new Vector2d(23.5, -10.75), Math.toRadians(0))
+                        // end tangent forms a nice curve
+                        .splineToConstantHeading(getVector2d(PowerPlayPoseStorageForPowerPlayDrive.RED_RIGHT_PARK_LOCATION_2), Math.toRadians(270))
+                        .build();
+
+                trajectoryToParkingLocation3 = robot.mecanum.trajectoryBuilder(trajectoryToJunctionPoleFromStart.end())
+                        .splineToConstantHeading(new Vector2d(23.5, -10.75), Math.toRadians(0))
+                        //.splineToSplineHeading(new Pose2d(47,-11.75, Math.toRadians(270)),Math.toRadians(0) )
+                        .splineToConstantHeading(getVector2d(PowerPlayPoseStorageForPowerPlayDrive.RED_RIGHT_PARK_LOCATION_3), Math.toRadians(0))
+                        .build();
+            }
+            break;
+        }
+    }
+
+    //*********************************************************************************************
+    //          MAJOR METHODS
+    //
+    // public methods that give the class its functionality
+    //*********************************************************************************************
+
+    @Override
+    public void start() {
+        currentState = States.START;
+        isComplete = false;
+        logCommand("start");
+    }
+
+    @Override
+    public void update() {
+        switch (currentState) {
+
+            case START: {
+                isComplete = false;
+                robot.mecanum.setPoseEstimate(startPose);
+                robot.mecanum.followTrajectory(trajectoryToJunctionPoleFromStart);
+                currentState = States.MOVING_TO_JUNCTION_POLE_FOR_SCORE;
+            }
+            break;
+
+            case MOVING_TO_JUNCTION_POLE_FOR_SCORE: {
+                if (!robot.mecanum.isBusy()) {
+                    currentState = States.READING_POLE_WITH_INVERSE_SENSOR;
+                    robot.distanceSensorForInverse.startAverage(4);
+                    //robot.coneGrabberArmController.moveToHighThenPrepareToRelease();
+                }
+            }
+            break;
+
+            case READING_POLE_WITH_INVERSE_SENSOR: {
+                if (robot.distanceSensorForInverse.isAverageReady()) {
+                    robot.distanceSensorForNormal.startAverage(4);
+                    currentState = States.READING_POLE_WITH_NORMAL_SENSOR;
+                    //robot.coneGrabberArmController.moveToHighThenPrepareToRelease();
+                }
+            }
+            break;
+
+            case READING_POLE_WITH_NORMAL_SENSOR: {
+                if (robot.distanceSensorForNormal.isAverageReady()) {
+                    currentState = States.COMPLETE;
+                    //robot.coneGrabberArmController.moveToHighThenPrepareToRelease();
+                }
+            }
+            break;
+
+            case RAISING_LIFT: {
+                if (robot.coneGrabberArmController.isCommandComplete()) {
+                    robot.leftLift.droppingOnPole();
+                    currentState = States.DROPPING_FOUR_INCHES;
+                }
+            }
+            break;
+
+            case DROPPING_FOUR_INCHES: {
+                if (robot.leftLift.isCommandComplete()) {
+                    robot.coneGrabberArmController.releaseThenMoveToPickup();
+                    currentState = States.RELEASING_OPEN_LIFT;
+                }
+            }
+            break;
+
+            case RELEASING_OPEN_LIFT: {
+                if (robot.coneGrabberArmController.isCommandComplete()) {
+                    if (trajectoryToParkingLocation == null) {
+                        currentState = States.COMPLETE;
+                    } else {
+                        robot.mecanum.followTrajectory(trajectoryToParkingLocation);
+                        currentState = States.MOVING_TO_PARKING;
+                    }
+
+                }
+            }
+            break;
+
+            case MOVING_TO_PARKING: {
+                if (!robot.mecanum.isBusy()) {
+                    currentState = States.COMPLETE;
+                }
+            }
+            break;
+
+            case COMPLETE: {
+                isComplete = true;
+                //robot.coneGrabber.close();
+                logCommand("finished");
+            }
+            break;
+        }
+    }
+}
---
+++ opmodes/PowerPlay/PowerPlayAutonomousTestSensor.java
@@ -0,0 +1,204 @@
+package org.firstinspires.ftc.teamcode.opmodes.PowerPlay;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.Configuration;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.MatchPhase;
+import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.Pipelines.SignalConePipeline;
+import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayAutonomousStateMachine;
+import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayAutonomousVisionTestSensor;
+import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayField;
+import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayGamepad;
+import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayPersistantStorage;
+import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayRobot;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.List;
+
+@Autonomous(name = "Autonomous Power Play - Test Sensor", group = "AA")
+//@Disabled
+
+public class PowerPlayAutonomousTestSensor extends LinearOpMode {
+
+    //*********************************************************************************************
+    //             Declarations
+    //*********************************************************************************************
+
+    public PowerPlayRobot robot;
+    public PowerPlayGamepad gamepad;
+    public Configuration config = null;
+    public PowerPlayField field;
+    private PowerPlayAutonomousStateMachine autonomousStateMachine;
+    private SignalConePipeline pipeline;
+    public SignalConePipeline.ConeColor coneColor;
+
+    private ElapsedTime timer;
+
+    DataLogging dataLog = null;
+
+    private Pose2d startPose;
+
+    @Override
+    public void runOpMode() {
+
+        //*********************************************************************************************
+        //  Initializations after the program is selected by the user on the driver phone
+        //*********************************************************************************************
+
+        // create the robot
+        telemetry.addData("Initializing ...", "Wait for it ...");
+        telemetry.update();
+
+        // set the persistant storage variable saying this is the teleop phase
+        PowerPlayPersistantStorage.setMatchPhase(MatchPhase.AUTONOMOUS);
+
+        // setup the log file
+        dataLog = new DataLogging("Autonomous", telemetry);
+        config = null;
+        config = new Configuration();
+        if (!config.load()) {
+            telemetry.addData("ERROR", "Couldn't load config file");
+            telemetry.update();
+        }
+        timer = new ElapsedTime();
+
+        // create the robot and run the init for it
+        robot = new PowerPlayRobot(hardwareMap, telemetry, config, dataLog, DistanceUnit.CM, this);
+        robot.createRobot();
+
+        // create the gamepad
+        gamepad = new PowerPlayGamepad(gamepad1, gamepad2, robot);
+
+        // create the power play field. This sets the locations for our particular alliance color
+        // and team location (left or right)
+        field = new PowerPlayField(PowerPlayPersistantStorage.getColorLocation());
+
+        // Here is where you create the state machine that is going to be run.
+        // Change this state machine out and the robot will do something different.
+        autonomousStateMachine = new PowerPlayAutonomousVisionTestSensor(robot, field, telemetry);
+        autonomousStateMachine.setDataLog(dataLog);
+        autonomousStateMachine.enableDataLogging();
+
+        // Allow reads of all of the motor data in one read.
+        enableBulkReads(hardwareMap, LynxModule.BulkCachingMode.AUTO);
+
+        // set the start location of the robot
+
+        if (PowerPlayPersistantStorage.getRobotPose() != null) {
+            startPose = PowerPlayPersistantStorage.getRobotPose();
+        } else {
+            startPose = field.getStartPose();
+        }
+
+        robot.mecanum.setPoseEstimate(startPose);
+        timer.reset();
+
+        // set a default parking location in case something goes wrong
+        PowerPlayPersistantStorage.setParkLocation(PowerPlayField.ParkLocation.ONE);
+
+        // put the webcam stuff here
+
+        // Create the pipeline to use to process the images coming from the webcam. It should be a
+        // statement like this:
+        pipeline = new SignalConePipeline(telemetry);
+
+        // start the webcam processing images through the pipeline.
+        robot.webcam.openCamera(OpenCvCameraRotation.UPRIGHT, pipeline);
+
+        // Wait for the start button
+
+        // If you have nothing to do while waiting for the start button to be pressed use:
+        // waitForStart();
+
+        // On the other hand, if you do have stuff to do (like display things on the driver station
+        // screen while a pipeline is running), use this:
+        while (!isStarted()) {
+            // get the current cone color from the pipeline
+            coneColor = pipeline.getConeColor();
+
+            telemetry.addData(">", "Press start to run Auto (make sure you ran the position setter first!)");
+            telemetry.addLine();
+            // display the alliance color and team location for the drivers to double check
+            telemetry.addData("Alliance color = ", PowerPlayPersistantStorage.getAllianceColor().toString());
+            telemetry.addData("Team Location  = ", PowerPlayPersistantStorage.getTeamLocation().toString());
+            telemetry.addLine();
+            // display the cone color that the web cam is seeing - live!
+            telemetry.addData("Cone color     = ", coneColor.toString());
+            telemetry.addData("Park location = ", PowerPlayPersistantStorage.getParkLocation().toString());
+            telemetry.addLine();
+            // display some pipeline info to use for debugging - just in case
+            pipeline.displayDebugTelemetry();
+            telemetry.update();
+
+            // update persistant storage with the current park location
+            PowerPlayPersistantStorage.setParkLocation(field.determineParkLocation(coneColor));
+            idle();
+        }
+
+        //*********************************************************************************************
+        //             Robot Running after the user hits play on the driver phone
+        //*********************************************************************************************
+
+        robot.loopTimer.startLoopTimer();
+        dataLog.logData("Auto start" );
+        timer.reset();
+        // Turn off the webcam and pipeline processing to save CPU cycles
+        robot.webcam.closeCamera();
+
+        // Get the cone grabber inside the robot so it does not get hit by anything
+        robot.coneGrabber.carryPosition();
+
+        // tell the autonomous what the signal cone said. The auto state machine will then
+        // pick the proper trajectory to the parking location.
+        autonomousStateMachine.setParkLocation(PowerPlayPersistantStorage.getParkLocation());
+
+        // Start the state machine
+        autonomousStateMachine.start();
+        while (opModeIsActive() && !autonomousStateMachine.isComplete()) {
+            autonomousStateMachine.update();
+            telemetry.addData("current state is", autonomousStateMachine.getCurrentState());
+            telemetry.update();
+            robot.update();
+            idle();
+        }
+
+        dataLog.logData("Auto complete in ", timer.milliseconds() );
+        //*************************************************************************************
+        //  Stop everything after the user hits the stop button on the driver phone
+        // ************************************************************************************
+
+        // Stop has been hit or auto is complete, shutdown everything. Note that some of the subsystem shutdowns may
+        // write to the datalog so we can't close it just yet.
+        //robot.shutdown();
+        dataLog.closeDataLog();
+        //telemetry.addData(">", "Done");
+        //telemetry.update();
+        telemetry.addData("Inverse Sensor= ", robot.distanceSensorForInverse.getAverageDistance(DistanceUnit.INCH));
+        telemetry.addData("Normal Sensor= ", robot.distanceSensorForNormal.getAverageDistance(DistanceUnit.INCH));
+        telemetry.update();
+        while (opModeIsActive() ) {
+            idle();
+        }
+    }
+
+    //*********************************************************************************************
+    //             Helper methods
+    //*********************************************************************************************
+
+    public void enableBulkReads(HardwareMap hardwareMap, LynxModule.BulkCachingMode mode) {
+        // set bulk read mode for the sensor reads - speeds up the loop
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(mode);
+        }
+    }
+}
+
+
