db496bcb463ad239554d2f753d8ddd6552dc14ea
TullyNYGuy <glenn_ball@utilitek.com>
2023-03-02 20:12:28 -0500

    Added code we created in test code, into the autonymous code

--- Lib/PowerPlayLib/PowerPlayCenterOnPole.java
+++ Lib/PowerPlayLib/PowerPlayCenterOnPole.java
@@ -4,9 +4,13 @@ package org.firstinspires.ftc.teamcode.Lib.PowerPlayLib;
 import android.util.Log;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.apache.commons.math3.analysis.integration.IterativeLegendreGaussIntegrator;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class PowerPlayCenterOnPole {
 
@@ -18,9 +22,10 @@ public class PowerPlayCenterOnPole {
     //*********************************************************************************************
     public enum States {
         START,
-        SEARCHING_FOR_JUNCTION,
+        FIXING_POSITION,
         WAITING_FOR_JUNCTION_TO_BE_CENTERED,
-        DONE
+        DONE,
+        WAIT_FOR_COMPLETE
     }
 
     private States currentState = States.START;
@@ -30,7 +35,9 @@ public class PowerPlayCenterOnPole {
     // y location where the robot start to search for the pole
     public static double START_SEARCH_LOCATION = 12;
     public static double SLOW_MOVEMENT = 0.2;
+    private Pose2d poleCenterLocation;
     private Pose2d location;
+    private ElapsedTime timer;
 
     //*********************************************************************************************
     //          PRIVATE DATA FIELDS AND SETTERS and GETTERS
@@ -49,6 +56,7 @@ public class PowerPlayCenterOnPole {
     public PowerPlayCenterOnPole(PowerPlayRobot robot, Telemetry telemetry) {
         this.robot = robot;
         this.telemetry = telemetry;
+        timer = new ElapsedTime();
     }
 
     //*********************************************************************************************
@@ -69,29 +77,17 @@ public class PowerPlayCenterOnPole {
         telemetry.addData("state = ", currentState.toString());
         switch (currentState) {
             case START: {
-                if (location.getX() > START_SEARCH_LOCATION) {
-                    //robot.mecanum.cancelFollowing();
-                    robot.mecanum.setWeightedDrivePower(
-                            new Pose2d(
-                                    SLOW_MOVEMENT,
-                                    0,
-                                    0
-                            )
-                    );
-                    robot.poleLocationDetermination.enablePoleLocationDetermination();
-                    currentState = States.SEARCHING_FOR_JUNCTION;
-                }
+                robot.poleLocationDetermination.enablePoleLocationDetermination();
+                currentState = States.WAITING_FOR_JUNCTION_TO_BE_CENTERED;
 
             }
             break;
-            case SEARCHING_FOR_JUNCTION: {
-                if (robot.poleLocationDetermination.getPoleLocation() == PowerPlayPoleLocationDetermination.PoleLocation.LEFT) {
-                    currentState = States.WAITING_FOR_JUNCTION_TO_BE_CENTERED;
-                }
-            }
-            break;
+
             case WAITING_FOR_JUNCTION_TO_BE_CENTERED: {
                 if (robot.poleLocationDetermination.getPoleLocation() == PowerPlayPoleLocationDetermination.PoleLocation.CENTER) {
+                    // found the pole
+                    poleCenterLocation = robot.mecanum.getPoseEstimate();
+                    robot.mecanum.cancelFollowing();
                     robot.mecanum.setWeightedDrivePower(
                             new Pose2d(
                                     0,
@@ -99,11 +95,45 @@ public class PowerPlayCenterOnPole {
                                     0
                             )
                     );
-                currentState = States.DONE;
+
+                    timer.reset();
+                    //robot.coneGrabberArmController.moveToHighThenPrepareToRelease();
+                    //currentState = States.RAISING_LIFT;
+                    currentState = States.FIXING_POSITION;
                 }
             }
             break;
+
+            case FIXING_POSITION: {
+                double distanceToMove = (162 - robot.poleLocationDetermination.getDistanceFromPole(DistanceUnit.MM)) / 25.4;
+                //logFile.logData("distance to move " +distanceToMove);
+                //move to the location needing to go
+                poleCenterLocation = robot.mecanum.getPoseEstimate();
+                //logFile.logData("robot position " +poleCenterLocation.getX() + " " + poleCenterLocation.getY());
+                Pose2d movement = new Pose2d(distanceToMove, 0, 0);
+                Pose2d newLocation = poleCenterLocation.plus(movement);
+                //logFile.logData("new position " +newLocation.getX() + " " + newLocation.getY());
+                Trajectory smallmove = robot.mecanum.trajectoryBuilder(poleCenterLocation)
+                        .lineTo(new Vector2d(newLocation.getX(), newLocation.getY()))
+                        .build();
+                //logFile.logData("current location = " + Double.toString(poleCenterLocation.getX()) + ", " + poleCenterLocation.getY());
+                //logFile.logData("fixed location = " + Double.toString(newLocation.getX()) + ", " + newLocation.getY());
+                robot.mecanum.followTrajectoryHighAccuracy(smallmove);
+                timer.reset();
+                //if done with movement, then move to next state
+                currentState = States.WAIT_FOR_COMPLETE;
+            }
+            break;
+
+            case WAIT_FOR_COMPLETE: {
+                if (!robot.mecanum.isBusy()) {
+                    currentState = States.DONE;
+                }
+            }
+
+            break;
             case DONE: {
+
                 // do nothing
             }
             break;
