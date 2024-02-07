23bb0b796728719f911a1bb7e0466583f0ed1896
TullyNYGuy <glenn_ball@utilitek.com>
2024-02-07 07:32:23 -0500

    fix setup for delivery

--- Lib/CenterStageLib/CenterStageWristServo.java
+++ Lib/CenterStageLib/CenterStageWristServo.java
@@ -36,7 +36,7 @@ public class CenterStageWristServo {
     private double highDropPosition = 0.64;
     private double mediumDropPosition = 0.64;
     private double lowDropPosition = 0.64;
-    private double setUpForDeliveryPosition = 0.75;
+    private double setUpForDeliveryPosition = 0.76;
 
     //*********************************************************************************************
     //          GETTER and SETTER Methods
@@ -92,7 +92,7 @@ public class CenterStageWristServo {
         wristServo.setPosition("lowDropPosition");
     }
 
-    public void setSetUpForDeliveryPosition() {
+    public void setUpForDeliveryPosition() {
         wristServo.setPosition("setUpForDeliveryPosition");
     }
 
