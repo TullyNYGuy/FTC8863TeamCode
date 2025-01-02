658bd22183d4c8244cec97a9c40e9f387c92634f
TullyNYGuy <glenn_ball@utilitek.com>
2025-01-01 22:33:47 -0500

    Setting up for real teleop

---
+++ Lib/IntoTheDeepLib/ITDBucketArm.java
@@ -0,0 +1,155 @@
+package org.firstinspires.ftc.teamcode.Lib.IntoTheDeepLib;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.Configuration;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogOnChange;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.FTCRobotSubsystem;
+
+public class ITDBucketArm implements FTCRobotSubsystem {
+
+    //*********************************************************************************************
+    //          ENUMERATED TYPES
+    //
+    // user defined types
+    //
+    //*********************************************************************************************
+    private enum ExtensionArmAndIntakeControllerState {
+        IDLE
+    }
+
+    private ExtensionArmAndIntakeControllerState state;
+
+    //*********************************************************************************************
+    //          PRIVATE DATA FIELDS AND SETTERS and GETTERS
+    //
+    // can be accessed only by this class, or by using the public
+    // getter and setter methods
+    //*********************************************************************************************
+    private ElapsedTime timer;
+
+    private DataLogging logFile;
+    private boolean loggingOn = false;
+    private DataLogOnChange logDataOnchange;
+
+    private boolean initComplete = false;
+    private final String INTAKE_SWEEPER_SERVO_NAME = "Sweeper Servo";
+
+
+    //*********************************************************************************************
+    //          Constructors
+    //
+    // the function that builds the class when an object is created
+    // from it
+    //*********************************************************************************************
+
+    public ITDBucketArm(HardwareMap hardwareMap, Telemetry telemetry) {
+
+        timer = new ElapsedTime();
+    }
+    //*********************************************************************************************
+    //          Helper Methods
+    //
+    // methods that aid or support the major functions in the class
+    //*********************************************************************************************
+
+    //*********************************************************************************************
+    //          MAJOR METHODS
+    //
+    // public methods that give the class its functionality
+    //*********************************************************************************************
+
+    //*********************************************************************************************
+    //          Commands
+    //*********************************************************************************************
+
+    /**
+     * Allow the intake to request the controller to move the extension arm out so an outtake can
+     * take place while the intake is not over the robot body.
+     */
+    public void intakeNeedsToOuttake(){
+
+    }
+
+    /**
+     * The intake says that it has transferred the sample to the bucket
+     */
+    public void intakeTransferComplete() {
+
+    }
+
+    //*********************************************************************************************
+    //          Housekeeping stuff
+    //*********************************************************************************************
+    @Override
+    public String getName() {
+        return INTAKE_SWEEPER_SERVO_NAME;
+    }
+
+    @Override
+    public boolean isInitComplete() {
+        if (initComplete) {
+            logCommand("Init complete");
+        }
+        return initComplete;
+    }
+
+    @Override
+    public boolean init(Configuration config) {
+        logCommand("Init starting");
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+    }
+
+    @Override
+    public void setDataLog(DataLogging logFile) {
+        this.logFile = logFile;
+        logDataOnchange = new DataLogOnChange(logFile);
+    }
+
+    @Override
+    public void enableDataLogging() {
+        this.loggingOn = true;
+    }
+
+    @Override
+    public void disableDataLogging() {
+        this.loggingOn = false;
+    }
+
+    private void logState() {
+        if (loggingOn && logFile != null) {
+            logDataOnchange.log(getName() + " state = " + state.toString());
+        }
+    }
+    private void logCommand(String command) {
+        if (loggingOn && logFile != null) {
+            logDataOnchange.log(getName() + " command = " + command);
+        }
+    }
+
+    public void displayState(Telemetry telemetry) {
+        //telemetry.addData("State = ", armIntakeState.toString());
+    }
+
+    @Override
+    public void timedUpdate(double timerValueMsec) {
+
+    }
+
+    //*********************************************************************************************
+    //          State machine
+    //*********************************************************************************************
+
+    @Override
+    public void update() {
+
+    }
+
+}
---
+++ opmodes/IntoTheDeep/ITDTeleop.java
@@ -0,0 +1,181 @@
+package org.firstinspires.ftc.teamcode.opmodes.IntoTheDeep;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Lib.IntoTheDeepLib.ITDGamepad;
+import org.firstinspires.ftc.teamcode.Lib.IntoTheDeepLib.ITDRobot;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.Configuration;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.DrivingMode;
+import org.firstinspires.ftc.teamcode.Lib.FTCLib.MecanumCommands;
+
+import java.util.List;
+
+@TeleOp(name = "Teleop ITD", group = "AA")
+//@Disabled
+
+public class ITDTeleop extends LinearOpMode {
+
+    //*********************************************************************************************
+    //             Declarations
+    //*********************************************************************************************
+
+    public ITDRobot robot;
+    public ITDGamepad gamepad;
+    public Configuration config = null;
+    //public ITDField field;
+
+    // public AutomaticTeleopFunctions automaticTeleopFunctions;
+    //set color for each game
+    //private FreightFrenzyStartSpot color = PersistantStorage.getStartSpot();
+    private ElapsedTime timer;
+
+    DataLogging dataLog = null;
+
+    private Pose2d startPose;
+
+    @Override
+    public void runOpMode() {
+
+        //*********************************************************************************************
+        //  Initializations after the program is selected by the user on the driver phone
+        //*********************************************************************************************
+
+        // create the robot
+        telemetry.addData("Initializing ...", "Wait for it ...");
+        telemetry.update();
+
+        // set the persistant storage variable saying this is the teleop phase
+//        PowerPlayPersistantStorage.setMatchPhase(MatchPhase.TELEOP);
+
+        dataLog = new DataLogging("Teleop", telemetry);
+        config = null;
+        config = new Configuration();
+        if (!config.load()) {
+//            telemetry.addData("ERROR", "Couldn't load config file");
+//            telemetry.update();
+        }
+        timer = new ElapsedTime();
+        MecanumCommands commands = new MecanumCommands();
+
+        robot = new ITDRobot(hardwareMap, telemetry, config, dataLog, DistanceUnit.CM, this);
+        //field = new ITDField(PowerPlayPersistantStorage.getColorLocation());
+
+        // create the robot and run the init for it
+        robot.createRobot();
+        gamepad = new ITDGamepad(gamepad1, gamepad2, robot);
+
+        enableBulkReads(hardwareMap, LynxModule.BulkCachingMode.AUTO);
+
+        //automaticTeleopFunctions = new AutomaticTeleopFunctions(robot, field, telemetry);
+
+//        if (PersistantStorage.robotPose != null) {
+//            startPose = PowerPlayPersistantStorage.getRobotPose();
+//        } else {
+//            startPose = field.getStartPose();
+//        }
+
+        // Setting the pose makes field centric drive 90 degrees out.
+        //robot.mecanum.setPoseEstimate(startPose);
+        timer.reset();
+
+        // set the driver joystick controls to either normal or inverted
+//        if (PowerPlayPersistantStorage.getTeamLocation() == TeamLocation.LEFT) {
+//            robot.robotModes.setDirectionSwap(ITDRobotModes.DirectionSwap.INVERSED);
+//        } else {
+//            // Right side of field
+//            robot.robotModes.setDirectionSwap(ITDRobotModes.DirectionSwap.NORMAL);
+//        }
+
+        // Wait for the start button
+
+        telemetry.addData(">", "Press start to run Teleop (make sure you ran the position setter first!)");
+        telemetry.update();
+
+        //robot.setColor(color);
+        waitForStart();
+
+        robot.loopTimer.startLoopTimer();
+
+        //*********************************************************************************************
+        //             Robot Running after the user hits play on the driver phone
+        //*********************************************************************************************
+        while (opModeIsActive()) {
+
+            // update the gamepad. It has the commands to be run when a button is pressed so the
+            // gamepad actually runs the robot commands.
+            gamepad.update();
+            // update the robot
+            robot.update();
+
+            // The following code uses road runner to move the robot in a driver (field) centric
+            // drive
+
+            telemetry.addData("Max Power = ", robot.robotModes.getCurrentMaxPower());
+            telemetry.addLine();
+
+            if (gamepad.getDrivingMode() == DrivingMode.ROBOT_CENTRIC) {
+                telemetry.addData("Direction swap = ", robot.robotModes.getDirectionSwap());
+                telemetry.addData("ROBOT CENTRIC driving", "!");
+
+                robot.mecanumDrive.calculateMotorCommandsRobotCentric(
+                        gamepad.gamepad1LeftJoyStickYValue * robot.robotModes.getDirectionSwapMultiplier(),
+                        gamepad.gamepad1LeftJoyStickXValue * robot.robotModes.getDirectionSwapMultiplier(),
+                        gamepad.gamepad1RightJoyStickXValue
+                );
+            }
+
+//            telemetry.addData("intake State = ", robot.intakeController.getState().toString());
+//            telemetry.addData("delivery Controller = ", robot.deliveryController.getState().toString());
+
+//            if (gamepad.getDrivingMode() == DrivingMode.FIELD_CENTRIC) {
+//                telemetry.addData("FIELD CENTRIC driving", "!");
+//                robot.mecanumDrive.calculateMotorCommandsFieldCentric(
+//                        gamepad.gamepad1LeftJoyStickYValue,
+//                        gamepad.gamepad1LeftJoyStickXValue,
+//                        gamepad.gamepad1RightJoyStickXValue
+//                );
+//            }
+
+            // feedback on the driver station
+
+            gamepad.displayGamepad1JoystickValues(telemetry);
+            telemetry.addData("", "");
+            telemetry.addData(">", "Press Stop to end.");
+            telemetry.update();
+
+            idle();
+        }
+
+        //*************************************************************************************
+        //  Stop everything after the user hits the stop button on the driver phone
+        // ************************************************************************************
+
+        // Stop has been hit, shutdown everything. Note that some of the subsystem shutdowns may
+        // write to the datalog so we can't close it just yet.
+        robot.shutdown();
+        dataLog.closeDataLog();
+        telemetry.addData(">", "Done");
+        telemetry.update();
+    }
+
+    //*********************************************************************************************
+    //             Helper methods
+    //*********************************************************************************************
+
+    public void enableBulkReads(HardwareMap hardwareMap, LynxModule.BulkCachingMode mode) {
+        // set bulk read mode for the sensor reads - speeds up the loop
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(mode);
+        }
+    }
+}
+
+
