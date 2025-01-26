380e94e8c976529906f7d2ebb5748aaa10e91380
TullyNYGuy <glenn_ball@utilitek.com>
2025-01-25 19:30:45 -0500

    Fix for bucket gate closing on setup for intake Move bucket arm to delivery before lift gets to final position

--- Lib/IntoTheDeepLib/ITDLift.java
+++ Lib/IntoTheDeepLib/ITDLift.java
@@ -22,9 +22,9 @@ public class ITDLift implements FTCRobotSubsystem {
     private enum LiftState {
         RESETING,
         MOVING,
-        MOVING_TO_READY_TO_DELIVER_POSITION,
         JOYSTICK_CONTROL,
-        IDLE
+        IDLE,
+        MOVING_TO_DELIVERY_POSITION
     }
 
     private LiftState state;
@@ -60,10 +60,11 @@ public class ITDLift implements FTCRobotSubsystem {
 
     private double initPosition = 0.0;
     private double transferPosition = 0;
-    private double startMovingBucketArmPosition = 21.0;
     private double readyToDeliverPosition = 23.0;
     private double lowBarHangPosition = 6.0;
     private double highBarHangPosition = 4.0;
+
+    private double positionToMoveBucketArm = 20;
 
 
     //*********************************************************************************************
@@ -193,7 +194,7 @@ public class ITDLift implements FTCRobotSubsystem {
                 logCommand("Delivery position");
                 readyToDeliverPositionAction();
                 controller.setLiftPositionReached(false);
-                state = LiftState.MOVING_TO_READY_TO_DELIVER_POSITION;
+                state = LiftState.MOVING_TO_DELIVERY_POSITION;
                 break;
             case MOVING:
             case RESETING:
@@ -378,16 +379,9 @@ public class ITDLift implements FTCRobotSubsystem {
                     state = LiftState.IDLE;
                 }
                 break;
-            case MOVING_TO_READY_TO_DELIVER_POSITION:
-                // even though the lift has not reached its final position, the bucket arm servo
-                // can be moved.
-                // 1 - the lift has slowed down enough that the acceleration will not
-                // damage the servo.
-                // 2 - the lift seems to hunt for its final position and waiting
-                // for it to get there delays the movement of the bucket arm to the delivery position.
-                if (lift.getCurrentPosition() >= startMovingBucketArmPosition) {
-                    // lie to the controller and tell it the lift has arrived at its final position
-                    // Well it will soon enough
+            case MOVING_TO_DELIVERY_POSITION:
+                if (lift.getCurrentPosition() >= positionToMoveBucketArm) {
+                    // tell the controller that the arm has reached its position
                     controller.setLiftPositionReached(true);
                     state = LiftState.IDLE;
                 }
