d1c60225b108e1cb6bc0f8f4dd2bd1019e0b0cd4
TullyNYGuy <glenn_ball@utilitek.com>
2025-01-27 23:18:57 -0500

    remove unused imports that were broken

--- Lib/IntoTheDeepLib/ITDAutoStateMachines.java
+++ Lib/IntoTheDeepLib/ITDAutoStateMachines.java
@@ -1,13 +1,6 @@
 package org.firstinspires.ftc.teamcode.Lib.IntoTheDeepLib;
 
 
-import static org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayField.getVector2d;
-import static org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayPoseStorage.RED_RIGHT_PARK_LOCATION_2;
-import static org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayPoseStorage.RED_RIGHT_PARK_LOCATION_3;
-
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -16,11 +9,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.AllianceColorTeamLocation;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogOnChange;
 import org.firstinspires.ftc.teamcode.Lib.FTCLib.DataLogging;
-import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayAutonomousStateMachine;
-import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayField;
-import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayPersistantStorage;
-import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayPoseStorageForPowerPlayDrive;
-import org.firstinspires.ftc.teamcode.Lib.PowerPlayLib.PowerPlayRobot;
 
 public class ITDAutoStateMachines {
 
