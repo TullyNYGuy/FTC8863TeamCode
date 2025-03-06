bea2931b3138d7988bf07dc589aac4054cdcce73
TullyNYGuy <glenn_ball@utilitek.com>
2025-03-05 22:24:23 -0500

    Start of the delivery height automatic adjuster

---
+++ Lib/IntoTheDeepLib/ITDDeliveryTracker.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Lib.IntoTheDeepLib;
+
+
+public class ITDDeliveryTracker {
+
+    //*********************************************************************************************
+    //          ENUMERATED TYPES
+    //
+    // user defined types
+    //
+    //*********************************************************************************************
+
+    //*********************************************************************************************
+    //          PRIVATE DATA FIELDS AND SETTERS and GETTERS
+    //
+    // can be accessed only by this class, or by using the public
+    // getter and setter methods
+    //*********************************************************************************************
+private int sampleCount = 0;
+
+public void deliveryOccured () {
+    sampleCount = sampleCount + 1;
+}
+    //*********************************************************************************************
+    //          Constructors
+    //
+    // the function that builds the class when an object is created
+    // from it
+    //*********************************************************************************************
+
+    //*********************************************************************************************
+    //          Helper Methods
+    //
+    // methods that aid or support the major functions in the class
+    //*********************************************************************************************
+
+    //*********************************************************************************************
+    //          MAJOR METHODS
+    //
+    // public methods that give the class its functionality
+    //*********************************************************************************************
+
+}
